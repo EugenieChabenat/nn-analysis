deb0a299067461ab41bc0ed9304cb03bbc8ae60c
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-24 14:25:38 -0400

    Update config.py

--- bin/config.py
+++ bin/config.py
@@ -17,18 +17,10 @@
     },
     {
         "model_names": [
-            "moco_control",
-            "moco_CD",
-            "moco_CF",
-            "moco_CDF",
             "barlow_control",
-            "barlow_CD",
-            "barlow_CF",
-            "barlow_CDF",
-            "barlow_P",
-            "barlow_PF",
+            "barlow_v2"
         ],
-        "epochs": [49,50],
+        "epochs": [19],
         "layers": [None], # All layers
         "metrics": [
             ["decode", 0],
@@ -43,69 +35,5 @@
             ["trajectory", 0],
         ]
     },
-    {
-        "model_names": [
-            "barlow_P",
-            "barlow_P_projector",
-        ],
-        "epochs": [82,83], # epoch 82
-        "layers": [None], # All layers
-        "metrics": [
-            # ["decode", 0],
-            # ["dimensionality", 0],
-            # ["fact", 0],
-            # ["generalize", 0],
-            # ["neural_fits", 0],
-            # ["rdm", 0],
-            # ["sparsity", 0],
-            ["curve", 0],
-            ["curve", 1],
-            # ["trajectory", 0],
-        ]
-    },
-    {
-        "model_names": [
-            "barlow_control",
-            "barlow_control_projector",
-        ],
-        "epochs": [54,55], # epoch 82
-        "layers": [None], # All layers
-        "metrics": [
-            # ["decode", 0],
-            # ["dimensionality", 0],
-            # ["fact", 0],
-            # ["generalize", 0],
-            # ["neural_fits", 0],
-            # ["rdm", 0],
-            # ["sparsity", 0],
-            ["curve", 0],
-            ["curve", 1],
-            # ["trajectory", 0],
-        ]
-    },
-    # {
-    #     "model_names": [
-    #         "moco_control",
-    #         "moco_CD",
-    #         "moco_CF",
-    #         "moco_CDF",
-    #         "barlow_control",
-    #         "barlow_CD",
-    #         "barlow_CF",
-    #         "barlow_CDF",
-    #         "barlow_P",
-    #         "barlow_PF",
-    #     ],
-    #     "epochs": [0,50],
-    #     "layers": [15,None], # 15, 16 layers
-    #     "metrics": [
-    #         ["decode", 0],
-    #         ["dimensionality", 0],
-    #         ["fact", 0],
-    #         ["generalize", 0],
-    #         ["neural_fits", 0],
-    #         ["rdm", 0],
-    #         ["sparsity", 0],
-    #     ]
-    # }
+
 ]
