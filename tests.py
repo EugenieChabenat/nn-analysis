a1a4286e80ded675f08aa59a4f1cf9f50e6ba659
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-24 12:59:20 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,14 @@
+import pickle
+
+
+path = '/mnt/smb/locker/issa-locker/users/hc3190/data/results/curve/OO/barlow_CD/0049/layer4.1.pkl' 
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+    
+print(data)
+
+path = '/mnt/smb/locker/issa-locker/users/Eugénie/data/hk2_cam_color_no_pca/OO/barlow_v2/0014/layer4.1/y.pkl' 
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+    
+print(data)
