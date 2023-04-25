a6c44f8f2e8e529474159a73a8fc67213aa9b927
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-25 10:14:08 -0400

    Update main_save.py

--- bin/py_scripts/main_save.py
+++ bin/py_scripts/main_save.py
@@ -106,13 +106,13 @@ def main(model_name, layers, acts_name, version, epoch=None, overwrite=False, de
 if __name__ == '__main__':
     print("Started main_save.py...")
     parser = argparse.ArgumentParser(description='Save activations')
-    parser.add_argument('model_name', type=str,
+    parser.add_argument('--model_name', default = "barlow_v2", type=str,
                         help='Specify the model of which activations are saved')
-    parser.add_argument('layers', type=int, nargs='+',
+    parser.add_argument('--layers',default= 16 type=int, nargs='+',
                         help='Specify the layers of which activations are saved')
-    parser.add_argument('acts_name', type=str,
+    parser.add_argument('--acts_name', default = "mkturk_test", type=str,
                         help='Specify what activations are saved')
-    parser.add_argument('version', type=int,
+    parser.add_argument('--version', default = 0,  type=int,
                         help='Version number. Error will be raised if the activations for the specified version' \
                              'already exists and --overwrite flag is not provided.')
     parser.add_argument('--epoch', type=int,
