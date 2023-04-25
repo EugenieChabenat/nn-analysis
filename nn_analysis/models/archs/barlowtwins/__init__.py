4bafda5650503973b27ef87e28f1705e3243aa2a
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-25 11:54:49 -0400

    Update __init__.py

--- nn_analysis/models/archs/barlowtwins/__init__.py
+++ nn_analysis/models/archs/barlowtwins/__init__.py
@@ -2,3 +2,4 @@ __all__ = ['barlowtwins', 'equivariant_all_bn']
 
 from .base import barlowtwins
 from .equivariant_all_bn import equivariant_all_bn
+from .equivariant_all_bn_v2 import equivariant_all_bn_v2
