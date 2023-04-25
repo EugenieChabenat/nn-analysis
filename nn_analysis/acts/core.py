c71f516e4207c9e92ca4f6fe447705a22ba04280
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-25 13:41:14 -0400

    Update core.py

--- nn_analysis/acts/core.py
+++ nn_analysis/acts/core.py
@@ -255,6 +255,8 @@ class Sampler(Processor):
         """
         assert self.configured
         assert tensor.ndim == 2
+        
+        print('indicies: ', self.indices)
         layer_indices = self.indices[layer_name]
         if tensor.is_cuda:
             layer_indices.to(tensor.get_device())
