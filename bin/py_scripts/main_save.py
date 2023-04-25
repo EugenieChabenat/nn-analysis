c20eb0d40f0ce5b66e7ad7c52116bf5d99c40cb4
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-25 10:04:06 -0400

    Update main_save.py

--- bin/py_scripts/main_save.py
+++ bin/py_scripts/main_save.py
@@ -9,8 +9,50 @@ model_configs = utils.load_config(MODEL_CONFIGS_PATH)
 arch_configs = utils.load_config(ARCH_CONFIGS_PATH)
 acts_configs = utils.load_config(ACTS_CONFIGS_PATH)
 env_config = utils.load_config(ENV_CONFIG_PATH)
-
+#---
+import importlib
+parser = argparse.ArgumentParser(description='Barlow Twins Training')
+parser.add_argument('name', type=str, metavar='NAME',
+                    help='name of experiment')
+parser.add_argument('--version', default=1, type=int, metavar='N',
+                    help='version of model')
+parser.add_argument('--dataset', default='imagenet', type=str,
+                    help='dataset on which to train the model')
+parser.add_argument('--port-id', default=58472, type=int, metavar='N',
+                    help='distributed training port number (default: 58472)')
+parser.add_argument('--workers', default=8, type=int, metavar='N',
+                    help='number of data loader workers')
+parser.add_argument('--epochs', default=1000, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--batch-size', default=4096, type=int, metavar='N',
+                    help='mini-batch size')
+parser.add_argument('--learning-rate', default=0.2, type=float, metavar='LR',
+                    help='base learning rate')
+parser.add_argument('--weight-decay', default=1e-6, type=float, metavar='W',
+                    help='weight decay')
+parser.add_argument('--lambd', default=0.0051, type=float, metavar='L',
+                    help='weight on off-diagonal terms')
+parser.add_argument('--projector', default='8192-8192-8192', type=str,
+                    metavar='MLP', help='projector MLP')
+parser.add_argument('--scale-loss', default=0.024, type=float,
+                    metavar='S', help='scale the loss')
+parser.add_argument('--print-freq', default=10, type=int, metavar='N',
+                    help='print frequency')
+parser.add_argument('--tensorboard', action='store_true',
+                    help='log training statistics to tensorboard')
+parser.add_argument('--no-flip', action='store_true',
+                    help='no random horizontal flips')
+#---
 def main(model_name, layers, acts_name, version, epoch=None, overwrite=False, debug=False, verbose=False):
+    
+    # --
+    args, other_argv = parser.parse_known_args()
+    other_args = importlib.import_module('.' + args.name, 'models').get_parser().parse_args(other_argv)
+    args = argparse.Namespace(**vars(args), **vars(other_args))
+    
+    Model = importlib.import_module('.' + args.name, 'models').Model
+    # --- 
+    
     arch = model_configs[model_name]['arch']
     layer_names = [arch_configs[arch]['layer_names'][layer] for layer in layers]
     acts_config = acts_configs[acts_name][f"{version:02d}"]
