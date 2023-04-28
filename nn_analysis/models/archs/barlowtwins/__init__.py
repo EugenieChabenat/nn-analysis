71c782c783bced1742941ef247c95b224a4984b0
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-28 14:16:07 -0400

    Update __init__.py

--- nn_analysis/models/archs/barlowtwins/__init__.py
+++ nn_analysis/models/archs/barlowtwins/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ['barlowtwins', 'equivariant_all_bn', 'equivariant_all_bn_v2']#, 'factorize_avgpool_equivariant_all_bn_v1']
+__all__ = ['barlowtwins', 'equivariant_all_bn', 'equivariant_all_bn_v2', 'factorize_avgpool_equivariant_all_bn_inj_v1']
 
 from .base import barlowtwins
 from .equivariant_all_bn import equivariant_all_bn
 from .equivariant_all_bn_v2 import equivariant_all_bn_v2
-#from .factorize_avgpool_equivariant_all_bn_v1 import factorize_avgpool_equivariant_all_bn_v1
+from .factorize_avgpool_equivariant_all_bn_inj_v1 import factorize_avgpool_equivariant_all_bn_inj_v1
