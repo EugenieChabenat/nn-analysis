2775318bf8be56da0ec4ef0d7a6c48d7528145e6
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-28 15:20:37 -0400

    Update straightening.py

--- straightening.py
+++ straightening.py
@@ -13,7 +13,7 @@ def load_data(metric, model_name, epoch, layers):
         return me.utils.load_data(model_name, epoch, layer_names, metric[0], metric[1])
       
 epoch = 29
-layers = np.arange(17)
+layers = np.arange(2)
 metric = ["curve", 1]
 metric_types = ["x_cam_trans", "y_cam_trans", "x_cam_rot", "y_cam_rot"]
 # metric_types = ["x_cam_rot", "x_focus_pan", "x_cam_pan"]
