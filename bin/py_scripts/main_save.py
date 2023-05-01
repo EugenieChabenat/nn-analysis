eea6d316546d141b4ce02a5595da3c074df6314f
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-01 10:02:43 -0400

    Update main_save.py

--- bin/py_scripts/main_save.py
+++ bin/py_scripts/main_save.py
@@ -58,10 +58,10 @@ def main(model_name, layers, acts_name, version, epoch=None, overwrite=False, de
     
     Model = importlib.import_module('.' + args.name, 'models').Model
     # --- 
-    layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    #layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     #layers = [0, 1, 2, 3, 4, 5, 6, 16]
     #layers = [0, 1, 2, 16]
-    #layers = [16]
+    layers = [0]
     arch = model_configs[model_name]['arch']
     layer_names = [arch_configs[arch]['layer_names'][layer] for layer in layers]
     acts_config = acts_configs[acts_name][f"{version:02d}"]
