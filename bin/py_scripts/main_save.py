9fcdd0a5fb42b95fbf880391c0f846017d84dd9b
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-02 10:46:55 -0400

    Update main_save.py

--- bin/py_scripts/main_save.py
+++ bin/py_scripts/main_save.py
@@ -62,7 +62,7 @@ def main(model_name, layers, acts_name, version, epoch=None, overwrite=False, de
     #layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     
     # v3 
-    layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
+    layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16]
     
     # v2 
     #layers = [0, 1, 2, 3, 4, 5, 6, 16]
