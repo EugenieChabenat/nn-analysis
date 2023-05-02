d0e3e93b70af10612d9fc7b238e4c79b48b5dec6
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-02 14:04:13 -0400

    Update neural_fits.py

--- neural_fits.py
+++ neural_fits.py
@@ -40,10 +40,44 @@ for i, metric_type in enumerate(metric_types):
     axes[0,i].set_title(metric_type)
     axes[0,i].legend()
 fig.supxlabel('layers')
-fig.supylabel('curvature')
+fig.supylabel('neural fits')
 fig.tight_layout()
 plt.show()
 plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/neural_fits/plot1.png')
+
+# -- 
+epoch = 29
+layers = np.arange(2)
+layers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
+metric = ["neural_fits", 0]
+metric_types = ['hvm-V4', 'hvm-IT', 'rust-V4', 'rust-IT']
+# metric_types = ["x_cam_rot", "x_focus_pan", "x_cam_pan"]
+model_names = [
+    #"barlow_v1_inj",
+    #"identity", 
+    "barlow_v3_equi", 
+    "barlow_control"
+]
+
+fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
+for i, metric_type in enumerate(metric_types):
+    for model_name in model_names:
+        print('model: ', model_name)
+        print('layer: ', layers)
+        
+        #print('keys: ', load_data(metric, model_name, epoch, one_layer).keys())
+        
+        scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
+        axes[0,i].plot(layers, scores, label=model_name)
+    #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
+    #axes[0,i].plot(layers, scores, label='identity')
+    axes[0,i].set_title(metric_type)
+    axes[0,i].legend()
+fig.supxlabel('layers')
+fig.supylabel('neural fits')
+fig.tight_layout()
+plt.show()
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/neural_fits/plot2.png')
 
 
 """epoch = 82
