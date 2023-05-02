aa3b9307d692a36626359602fe8922585ece6900
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-02 14:24:27 -0400

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from nn_analysis import metrics as me
+from nn_analysis import utils
+from nn_analysis import plot as pt
+
+def load_data(metric, model_name, epoch, layers):
+    layer_names = utils.get_layer_names(model_name, layers)
+    if isinstance(layer_names, list):
+        return [me.utils.load_data(model_name, epoch, layer_name, metric[0], metric[1]) for layer_name in layer_names]
+    else:
+        return me.utils.load_data(model_name, epoch, layer_names, metric[0], metric[1])
+      
+epoch = 29
+layers = np.arange(2)
+layers =[0, 1, 2, 3, 4, 5]
+one_layer = 0
+metric = ["fact", 0]
+metric_types = ['']
+model_names = [
+    #"barlow_v1_inj",
+    #"identity", 
+    "barlow_v2_inj", 
+    "barlow_control"
+]
+
+fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
+for i, metric_type in enumerate(metric_types):
+    for model_name in model_names:
+        print('model: ', model_name)
+        print('layer: ', layers)
+        
+        print('keys: ', load_data(metric, model_name, epoch, one_layer).keys())
+        
+        scores = [load_data(metric, model_name, epoch, layer) for layer in layers]
+        axes[0,i].plot(layers, scores, label=model_name)
+    #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
+    #axes[0,i].plot(layers, scores, label='identity')
+    axes[0,i].set_title(metric_type)
+    axes[0,i].legend()
+fig.supxlabel('layers')
+fig.supylabel('fact')
+fig.tight_layout()
+plt.show()
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/plot1.png')
+
+# -- 
+epoch = 29
+layers = np.arange(2)
+layers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
+metric = ["dimensionality", 0]
+metric_types = ['']
+model_names = [
+    #"barlow_v1_inj",
+    #"identity", 
+    "barlow_v3_equi", 
+    "barlow_control"
+]
+
+fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
+for i, metric_type in enumerate(metric_types):
+    for model_name in model_names:
+        print('model: ', model_name)
+        print('layer: ', layers)
+        
+        #print('keys: ', load_data(metric, model_name, epoch, one_layer).keys())
+        
+        scores = [load_data(metric, model_name, epoch, layer) for layer in layers]
+        axes[0,i].plot(layers, scores, label=model_name)
+    #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
+    #axes[0,i].plot(layers, scores, label='identity')
+    axes[0,i].set_title(metric_type)
+    axes[0,i].legend()
+fig.supxlabel('layers')
+fig.supylabel('fact')
+fig.tight_layout()
+plt.show()
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/plot2.png')
