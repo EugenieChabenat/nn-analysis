481e369d6a9cce611725303627d9320cdd31ba3e
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-03 11:37:21 -0400

    Update main_analyze.py

--- bin/py_scripts/main_analyze.py
+++ bin/py_scripts/main_analyze.py
@@ -22,10 +22,10 @@ def main(model_name, layers, metric_name, version, epoch=None, overwrite=False, 
     print('layer names: ', layer_names)
     
     # complete 
-    #layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     
     # v3 
-    layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
+    #layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
     
     # v1
     #layers = [0, 1, 2, 16]
