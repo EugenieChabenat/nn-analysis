33af7aab97e3e80426f128ad008e1057164b575e
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-04 16:09:55 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -86,55 +86,22 @@ model_names = [
     "barlow_v1_inj_b", 
     "barlow_control"
 ]
-fig, axes = subplots(1, 1, height_per_plot=7.5, width_per_plot=7.5, polar=True)
+fig, axes = pt.round_plot.subplots(1, 1, height_per_plot=7.5, width_per_plot=7.5, polar=True)
 ax = axes[0,0]
 
 x = metric_types
 for i, model_name in enumerate(model_names):
     #y = np.array([results[model_name][metric][-1,0] for metric in metrics])
     y = np.array([load_data(metric, model_name, epoch, one_layer)[metric_type] for metric_type in metric_types])
-    r_plot(ax, x, y, label=model_names[i])
-r_xticks(ax, x, x_offset=0.3, y_offset=0.3, size=11, color="grey")
-r_yticks(ax, min=0.0, max=1.0, steps=4)
-r_legend(ax, loc=(1.0, 1.0))
+    pt.round_plot.r_plot(ax, x, y, label=model_names[i])
+pt.round_plot.r_xticks(ax, x, x_offset=0.3, y_offset=0.3, size=11, color="grey")
+pt.round_plot.r_yticks(ax, min=0.0, max=1.0, steps=4)
+pt.round_plot.r_legend(ax, loc=(1.0, 1.0))
 fig.tight_layout()
-savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FINAL_rond.png')
+pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FINAL_rond.png')
 fig.show()
 # -- 
 
 
 
-"""epoch = 29
-layers = np.arange(2)
-layers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
-metric = ["decode", 0]
-metric_types = ['obj_class', 'cam_pos_x', 'cam_pos_y', 'cam_scale', #'brightness', 'contrast', 'saturation', 'hue', 
-                'obj_pos_x', 'obj_pos_y', 'obj_scale']#, 
-                #'obj_pose_x', 'obj_pose_y', 'obj_pose_z', 'cam_pos', 'obj_pos', 'color', 'lighting', 'obj_pose']# metric_types = ["x_cam_rot", "x_focus_pan", "x_cam_pan"]
-model_names = [
-    #"barlow_v1_inj",
-    #"identity", 
-    "barlow_v3_equi", 
-    "barlow_control"
-]
-
-fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
-for i, metric_type in enumerate(metric_types):
-    for model_name in model_names:
-        print('model: ', model_name)
-        print('layer: ', layers)
-        
-        #print('keys: ', load_data(metric, model_name, epoch, one_layer).keys())
-        
-        scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
-        axes[0,i].plot(layers, scores, label=model_name)
-    #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
-    #axes[0,i].plot(layers, scores, label='identity')
-    axes[0,i].set_title(metric_type)
-    axes[0,i].legend()
-fig.supxlabel('layers')
-fig.supylabel('neural fits')
-fig.tight_layout()
-plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/plot2.png')"""
 
