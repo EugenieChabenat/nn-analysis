3709a2c7de8cc360300de751cf18c40097c99f2c
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-04 16:08:03 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -13,52 +13,7 @@ def load_data(metric, model_name, epoch, layers):
         return [me.utils.load_data(model_name, epoch, layer_name, metric[0], metric[1]) for layer_name in layer_names]
     else:
         return me.utils.load_data(model_name, epoch, layer_names, metric[0], metric[1])
-# --
-def savefig(fig, filename):
-    folder = os.path.split(filename)[0]
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    fig.savefig(filename)
-    
-def subplots(n_rows, n_cols, height_per_plot=4, width_per_plot=5, polar=False, **kwargs):
-    fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols*width_per_plot, n_rows*height_per_plot), subplot_kw={'polar': polar}, facecolor='white', **kwargs)
-    if n_rows == 1 and n_cols == 1:
-        axes = np.array([[axes]])
-    elif n_rows == 1:
-        axes = axes.reshape((1,-1))
-    elif n_cols == 1:
-        axes = axes.reshape((-1,1))
-    else:
-        pass
-    return fig, axes
 
-def r_plot(ax, x, y, color=None, label=None, **kwargs):
-    N = len(x)
-    if N == 0:
-        angles, values = [], []
-    else:
-        angles = [n / float(N) * 2 * np.pi for n in range(N)]
-        angles = angles + angles[:1]
-        values = list(y)
-        values = values + values[:1]
-    ax.set_rlabel_position(2.0)
-    line1, = ax.plot(angles, values, linewidth=1, linestyle='solid', color=color, label=label, **kwargs)
-#     line2, = ax.fill(angles, values, alpha=0.1, color=color, **kwargs)
-    return line1,
-
-def r_xticks(ax, x, x_offset=0.15, y_offset=0.1, size=11, color="grey", **kwargs):
-    N = len(x)
-    angles = [n / float(N) * 360 for n in range(N)]
-    _, labels = ax.set_thetagrids(angles, x, color=color, size=size)
-    for i, label in enumerate(labels):
-        x, y = label.get_position()
-        label.set_position((x,y-x_offset*np.cos(angles[i]*np.pi/180)**2-y_offset*(np.exp(-np.abs(angles[i]*np.pi/180-3*np.pi/2))+np.exp(-np.abs(angles[i]*np.pi/180-np.pi/2)))**5))
-
-def r_yticks(ax, min=0.0, max=1.0, steps=4):
-    ax.set_rgrids(np.linspace(min, max, steps+1)[1:], color="grey", size=9)
-    ax.set_ylim((min, max))
-def r_legend(ax, loc=(1.0, 0.6), **kwargs):
-    ax.legend(loc=loc, **kwargs)
 
 
 # --- 
@@ -93,11 +48,11 @@ model_names = [
     "barlow_control"
 ]
 
-"""fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
+fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
 for i, metric_type in enumerate(metric_types):
     for model_name in model_names:
-        print('model: ', model_name)
-        print('layer: ', layers)
+        #print('model: ', model_name)
+        #print('layer: ', layers)
                 
         scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
         if model_name == "barlow_v1_inj_b": 
@@ -112,7 +67,7 @@ fig.supxlabel('layers')
 fig.supylabel('decode')
 fig.tight_layout()
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FACES_inj_vs_control_obj_class.png')"""
+#plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FACES_inj_vs_control_obj_class.png')
 
 
 
@@ -143,7 +98,7 @@ r_xticks(ax, x, x_offset=0.3, y_offset=0.3, size=11, color="grey")
 r_yticks(ax, min=0.0, max=1.0, steps=4)
 r_legend(ax, loc=(1.0, 1.0))
 fig.tight_layout()
-savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/rond.png')
+savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FINAL_rond.png')
 fig.show()
 # -- 
 
