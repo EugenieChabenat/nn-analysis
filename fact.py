d1df79a4dcf8126e7d757eacc30951f9c3b35016
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-04 16:05:38 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -67,6 +67,13 @@ one_layer = 16
 fig, axes = pt.round_plot.subplots(1, 1, height_per_plot=7.5, width_per_plot=7.5, polar=True)
 ax = axes[0,0]
 
+model_names = [
+    "barlow_v1_inj",
+    "barlow_v2_inj", 
+    "barlow_v1_inj_b",
+    "barlow_control"
+]
+
 metric_types = ['ss_inv-background', 'ss_inv-obj_motion', 'ss_inv-crop','ss_inv-color','inv-background', 'inv-obj_motion', 'inv-crop', 'inv-color',
                 'fact-background', 'fact-obj_motion', 'fact-crop', 'fact-color']
 
@@ -79,39 +86,7 @@ pt.round_plot.r_xticks(ax, x, x_offset=0.3, y_offset=0.3, size=11, color="grey")
 pt.round_plot.r_yticks(ax, min=0.0, max=1.0, steps=4)
 pt.round_plot.r_legend(ax, loc=(1.0, 1.0))
 fig.tight_layout()
-pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/rond.png')
+pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/FINAL_rond.png')
 fig.show()
 
-"""epoch = 29
-layers = np.arange(2)
-layers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
-metric = ["fact", 0]
-metric_types = ['ss_inv-obj_motion', 'fact-obj_motion', 'inv-obj_motion', 'ss_inv-background', 
-                'fact-background', 'inv-background', 'ss_inv-crop', 'fact-crop', 'inv-crop', 'ss_inv-color', 'fact-color', 'inv-color']
 
-model_names = [
-    #"barlow_v1_inj",
-    #"identity", 
-    "barlow_v3_equi", 
-    "barlow_control"
-]
-
-fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
-for i, metric_type in enumerate(metric_types):
-    for model_name in model_names:
-        print('model: ', model_name)
-        print('layer: ', layers)
-        
-        #print('keys: ', load_data(metric, model_name, epoch, one_layer).keys())
-        
-        scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
-        axes[0,i].plot(layers, scores, label=model_name)
-    #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
-    #axes[0,i].plot(layers, scores, label='identity')
-    axes[0,i].set_title(metric_type)
-    axes[0,i].legend()
-fig.supxlabel('layers')
-fig.supylabel('fact')
-fig.tight_layout()
-plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/plot2.png')"""
