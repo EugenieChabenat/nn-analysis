29899b58e3f174b4e869f83f07112602fa515bd5
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-08 13:47:02 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -25,9 +25,9 @@ metric = ["decode", 0]
 
 #metric_types = ['obj_class']
                 
-metric_types = ['cam_pos_x', 'cam_pos_y', 'cam_scale', 'cam_pos']
+#metric_types = ['cam_pos_x', 'cam_pos_y', 'cam_scale', 'cam_pos']
                 
-#metric_types = ['brightness', 'contrast', 'saturation', 'hue', 'color', 'lighting']
+metric_types = ['brightness', 'contrast', 'saturation', 'hue', 'color', 'lighting']
                 
 #metric_types = ['obj_pos_x', 'obj_pos_y', 'obj_scale', 'obj_pos'] 
 
@@ -69,7 +69,7 @@ fig.supxlabel('layers')
 fig.supylabel('decode')
 fig.tight_layout()
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/3FACES_inj_vs_control_cam_pos&scale.png')
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/3FACES_inj_vs_control_color&light.png')
 
 
 
