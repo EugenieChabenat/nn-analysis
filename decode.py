03e71bcfa46c4c63da16c03286ad36d316856933
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-10 10:39:24 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -164,6 +164,8 @@ model_names = [
     "barlow_v1_inj_b", 
 ]
 
+one_layer = 3
+
 fig, axes = pt.round_plot.subplots(1,3,height_per_plot=6,width_per_plot=6)
 for i, model_name in enumerate(model_names):
     #ys = [[results[model_name][metric][-1,0]-results[baseline_model_name][metric][-1,0] for metric in metrics] for metrics in metricss]
@@ -182,7 +184,7 @@ y_lim_max = max([axes[0,i].get_ylim()[1] for i in range(len(model_names))])
 for i in range(len(model_names)):
     axes[0,i].set_ylim(y_lim_min, y_lim_max)
 fig.tight_layout()
-pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FINAL_hist.png')
+pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/FINAL_hist_layer3.png')
 fig.show()
 
 
