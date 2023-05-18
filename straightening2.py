9bb61d3d30f2f8ea88afc8324e6a09eea599fb85
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-18 14:48:11 -0400

    Update straightening2.py

--- straightening2.py
+++ straightening2.py
@@ -59,7 +59,7 @@ fig.supxlabel('layers')
 fig.supylabel('curvature')
 fig.tight_layout()
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/natural_movies/512two_metrics_plot2.png')
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/natural_movies/512two_metrics_plot4.png')
 
 
 
