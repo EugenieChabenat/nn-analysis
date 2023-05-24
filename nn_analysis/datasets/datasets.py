6118cdcb190d2883794092274989e9e841b24f39
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-24 16:51:59 -0400

    Update datasets.py

--- nn_analysis/datasets/datasets.py
+++ nn_analysis/datasets/datasets.py
@@ -57,13 +57,8 @@ def get_dataset(dataset_name, split='train', **kwargs):
         dataset.class_to_idx = class_to_idx
         
     elif dataset_name == 'rust':
-        #images = torch.from_numpy(np.load('/mnt/smb/locker/issa-locker/users/Eugénie/datasets/artificial_matrix.npy'))
-        #print('shape: ', images.shape)
-        #images = images.reshape((2, 110, 512, 512))
-        #images = images[0, :, :, :]
         
-        #images = torch.from_numpy(np.load(os.path.join(env_config['rust_path'], 'Rust_images.npy')))
-        print('image rust shape: ', images.shape)
+        images = torch.from_numpy(np.load(os.path.join(env_config['rust_path'], 'Rust_images.npy')))
         images = [T.ToPILImage()(images[i]).convert("RGB") for i in range(images.shape[0])]
         print('image rust shape: ', len(images))
         targets = np.arange(len(images))
