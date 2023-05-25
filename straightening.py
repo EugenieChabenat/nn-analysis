8331e62b70c01fda0d7faafbdfac616e4b9b9f75
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-25 11:30:29 -0400

    Update straightening.py

--- straightening.py
+++ straightening.py
@@ -42,8 +42,8 @@ metric = ["curve", 1]
 #metric_types = ["x_cam_trans", "y_cam_trans", "x_cam_rot", "y_cam_rot"]
 #metric_types = ['x_pan-detailed', 'x_pan', 'y_pan-detailed', 'y_pan', 'z_pan-detailed', 'z_pan']
 
-#metric_types = ['x_focus_pan-detailed', 'x_focus_pan', 
-metric_types = ['y_focus_pan-detailed', 'y_focus_pan'] 
+metric_types = ['x_focus_pan-detailed', 'x_focus_pan', 
+#metric_types = ['y_focus_pan-detailed', 'y_focus_pan'] 
 #metric_types = ['z_focus_pan-detailed', 'z_focus_pan']
 
 #metric_types = ['x_cam_pan-detailed', 'x_cam_pan', 
@@ -107,7 +107,7 @@ fig.supxlabel('layers')
 fig.supylabel('curvature')
 fig.tight_layout()
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/sanity_control/plot2.png')
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/sanity_control/plot1.png')
 
 
 
