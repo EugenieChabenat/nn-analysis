6659a6bd7699d3f15fefbbc97f1a3fdb019226bf
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-05-25 15:19:20 -0400

    Update straightening.py

--- straightening.py
+++ straightening.py
@@ -39,10 +39,10 @@ epoch = 29
 #layers = np.arange(12)
 layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 metric = ["curve", 1]
-metric_types = ["x_cam_trans", "y_cam_trans", "x_cam_rot", "y_cam_rot"]
+#metric_types = ["x_cam_trans", "y_cam_trans", "x_cam_rot", "y_cam_rot"]
 #metric_types = ['x_pan-detailed', 'x_pan', 'y_pan-detailed', 'y_pan', 'z_pan-detailed', 'z_pan']
 
-#metric_types = ['x_focus_pan-detailed', 'x_focus_pan'] 
+metric_types = ['x_focus_pan-detailed', 'x_focus_pan'] 
 #metric_types = ['y_focus_pan-detailed', 'y_focus_pan'] 
 #metric_types = ['z_focus_pan-detailed', 'z_focus_pan']
 
@@ -108,7 +108,7 @@ fig.supxlabel('layers')
 fig.supylabel('curvature')
 fig.tight_layout()
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/with_IT/plot0.png')
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/with_IT/plot1.png')
 
 
 
