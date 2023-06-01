4278699bad6c82936580f520a51e1481ab97b169
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-01 16:59:05 -0400

    Update base.py

--- nn_analysis/models/archs/barlowtwins/base.py
+++ nn_analysis/models/archs/barlowtwins/base.py
@@ -21,7 +21,7 @@ class BarlowTwins(nn.Module):
         #sizes = [2048] + list(map(int, args.projector.split('-')))
         sizes = [256] + list(map(int, args.projector.split('-')))
         sizes = [512] + list(map(int, args.projector.split('-')))
-        #sizes = [1024] + list(map(int, args.projector.split('-')))
+        sizes = [1024] + list(map(int, args.projector.split('-')))
         layers = []
         for i in range(len(sizes) - 2):
             layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=False))
