ca8f1489ac3ad0f14b6cba73e7cb50e86b889188
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-01 14:07:53 -0400

    Update straightening.py

--- straightening.py
+++ straightening.py
@@ -99,7 +99,7 @@ model_names = [
 # ------------------------------------------------------------------------------------
 for key, metric_types in list_metrics.items(): 
 
-    fig, axes = pt.core.subplots(1, len(metric_types), size=(5,4), sharex=True)
+    fig, axes = pt.core.subplots(1, len(metric_types), size=(10,8), sharex=True)
     for i, metric_type in enumerate(metric_types):
         for model_name in model_names:
             print('model: ', model_name)
