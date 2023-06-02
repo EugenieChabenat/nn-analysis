e072bef53ca5c64b35a3fb8abc4993c5849472a8
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-02 11:05:22 -0400

    Update straightening_details.py

--- straightening_details.py
+++ straightening_details.py
@@ -51,31 +51,31 @@ list_metrics = {
 }
 
 list_labels = {
-    "barlow_twins_50epochs_pc1": "max PCs",
-    "barlow_twins_50epochs_pc2": "300 PCs", 
-    "barlow_twins_50epochs_pc3": "300 RCs",
-    "barlow_twins_50epochs_pc4": "500 RCs", 
-    "barlow_twins_50epochs_pc5": "1000 RCs", 
-    "barlow_twins_50epochs_pc6": "2048 RCs"
+    "injection_v1_pc1": "max PCs",
+    "injection_v1_pc2": "300 PCs", 
+    "injection_v1_pc3": "300 RCs",
+    "injection_v1_pc4": "500 RCs", 
+    "injection_v1_pc5": "1000 RCs", 
+    "injection_v1_pc6": "2048 RCs"
 }
 
 dict_color = {
-    "barlow_twins_50epochs_pc1" : "red",
-    "barlow_twins_50epochs_pc2": "green", 
-    "barlow_twins_50epochs_pc3": "violet",
-    "barlow_twins_50epochs_pc4": "purple", 
-    "barlow_twins_50epochs_pc5": "indigo", 
-    "barlow_twins_50epochs_pc6": "plum"
+    "injection_v1_pc1" : "red",
+    "injection_v1_pc2": "green", 
+    "injection_v1_pc3": "violet",
+    "injection_v1_pc4": "purple", 
+    "injection_v1_pc5": "indigo", 
+    "injection_v1_pc6": "plum"
 }
 
 
 model_names = [
-    "barlow_twins_50epochs_pc1",
-    "barlow_twins_50epochs_pc2", 
-    "barlow_twins_50epochs_pc3",
-    "barlow_twins_50epochs_pc4", 
-    "barlow_twins_50epochs_pc5", 
-    "barlow_twins_50epochs_pc6", 
+    "injection_v1_pc1",
+    "injection_v1_pc2", 
+    "injection_v1_pc3",
+    "injection_v1_pc4", 
+    "injection_v1_pc5", 
+    "injection_v1_pc6", 
     #"barlow_twins_50epochs_pc7"
 ]
 
@@ -124,8 +124,8 @@ for key, metric_types in list_metrics.items():
     fig.supylabel('curvature')
     fig.tight_layout()
     plt.show()
-    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/pcs_injection_bt50_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/PCs_vs_RCs_injection_bt50_{}.png'.format(key))
+    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/pcs_injection_v1_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/PCs_vs_RCs_injection_v1_{}.png'.format(key))
     #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/PCs_vs_RCs/injection_v1.png')
 
 
