9768b35cbfb432eff5b5d44125af6dc34cc24283
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-01 21:41:26 -0400

    Update acts.py

--- nn_analysis/acts/acts.py
+++ nn_analysis/acts/acts.py
@@ -87,9 +87,9 @@ class Acts:
             n_pcs = 300
             print('n_pcs: ', n_pcs)
             # if we compute pcs 
-            y, evr = utils.get_pcs(y, n_pcs, svd_solver=svd_solver)
-            return {'y': y, 'evr': evr}
+            """y, evr = utils.get_pcs(y, n_pcs, svd_solver=svd_solver)
+            return {'y': y, 'evr': evr}"""
             # if we return random components 
-            #y = utils.get_random_components(y, n_rcs=500)
-            #return {'y': y}
+            y = utils.get_random_components(y, n_rcs=300)
+            return {'y': y}
         return {'y': y}
