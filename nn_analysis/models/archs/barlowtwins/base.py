161162a4361a81bf9d1ab005cd427097e88f181d
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-02 14:55:09 -0400

    Update base.py

--- nn_analysis/models/archs/barlowtwins/base.py
+++ nn_analysis/models/archs/barlowtwins/base.py
@@ -19,7 +19,7 @@ class BarlowTwins(nn.Module):
 
         # projector
         sizes = [2048] + list(map(int, args.projector.split('-')))
-        sizes = [256] + list(map(int, args.projector.split('-')))
+        #sizes = [256] + list(map(int, args.projector.split('-')))
         #sizes = [512] + list(map(int, args.projector.split('-')))
         #sizes = [1024] + list(map(int, args.projector.split('-')))
         layers = []
