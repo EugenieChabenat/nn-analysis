95e57fa20558c7c2acc7aa81edd7b06e88c6f9c2
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-02 14:59:35 -0400

    Update main_analyze.py

--- bin/py_scripts/main_analyze.py
+++ bin/py_scripts/main_analyze.py
@@ -53,7 +53,7 @@ def main(model_name, layers, metric_name, version, epoch=None, overwrite=False, 
 if __name__ == '__main__':
     print("Started main_analyze.py...")
     parser = argparse.ArgumentParser(description='Analyze activations')
-    parser.add_argument('--model_name', default= "injection_v1_nm6",  type=str,
+    parser.add_argument('--model_name', default= "barlow_twins_50epochs_nm6",  type=str,
                         help='specify the model of which activations are saved')
     parser.add_argument('--layers', default = 0, type=int, nargs='+',
                         help='specify the layers of which activations are saved')
