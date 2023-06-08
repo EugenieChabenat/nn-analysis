cce5366d8a8086fbe5fb40dcd3ff51a7ec73bba1
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-08 09:37:27 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -36,15 +36,16 @@ metric_dict = {'obj_class': 'Object Class',
                }
 
 dict_color = {
-    "injection_v1" : "blue",
-    "injection_v2": "red", 
-    "injection_v4": "orange",
-    "injection_IT": "green",
-    "injection_conv_v1": "red", 
-    "v4_no_injection": "purple", 
-    "resnet50_untrained": "pink", 
-    "barlow_twins_50epochs": "grey", 
-    "barlow_fact_no_injection": "black"
+    "injection_v1" : ["blue", '--'],
+    "injection_v2": ["red", '--'], 
+    "injection_v4": ["orange", '--'],
+    "injection_IT": ["green", '--'],
+    "injection_conv_v1": ["red", '-'], 
+    "injection_conv_v4": ["red", '-'], 
+    "v4_no_injection": ["purple", '--'], 
+    "resnet50_untrained": ["pink", '--'], 
+    "barlow_twins_50epochs": ["grey", '--'], 
+    "barlow_fact_no_injection": ["black", '--']
 }
 # --- 
 epoch = 29
@@ -76,12 +77,13 @@ list_metrics = {
     "Object Pose 2": ['obj_pose_z', 'obj_pose']
 }
 model_names = [
-    "injection_v1",
-    "injection_v2", 
+    #"injection_v1",
+    #"injection_v2", 
     "injection_v4",
-    "injection_IT",
-    "injection_conv_v1", 
-    "v4_no_injection", 
+    #"injection_IT",
+    #"injection_conv_v1", 
+    "injection_conv_v4", 
+    #"v4_no_injection", 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
     "barlow_fact_no_injection"
@@ -104,9 +106,9 @@ for key, metric_types in list_metrics.items():
         for model_name in model_names:
             scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
             if model_name == "barlow_v1_inj_b": 
-                axes[0,i].plot(layers, scores, label="barlow_v3_inj", color = dict_color[model_name])
+                axes[0,i].plot(layers, scores, label="barlow_v3_inj", color = dict_color[model_name][0], ls = dict_color[model_name][1])
             else: 
-                axes[0,i].plot(layers, scores, label=model_name, color = dict_color[model_name])
+                axes[0,i].plot(layers, scores, label=model_name, color = dict_color[model_name][0], ls = dict_color[model_name][1])
         #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
         #axes[0,i].plot(layers, scores, label='identity')
         
@@ -132,7 +134,7 @@ for key, metric_types in list_metrics.items():
     fig.supylabel('decode')
     fig.tight_layout()
     plt.show()
-    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
+    #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
     plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/{}.png'.format(key))
 
 
