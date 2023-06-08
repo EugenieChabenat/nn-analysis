3d4df7ed4936639c0794e7b8d6484606693bad78
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-08 11:10:08 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -28,10 +28,11 @@ metric_dict = {'ss_inv-background': 'SS Invariance - Background',
               }
 dict_color = {
     "injection_v1" : ["blue", '--'],
-    "injection_v2": ["red", '--'], 
+    "injection_v2": ["orange", '-'], 
     "injection_v4": ["orange", '-'],
     "injection_IT": ["green", '--'],
     "injection_conv_v1": ["red", '-'], 
+    "injection_conv_v2": ["red", '-'], 
     "injection_conv_v4": ["red", '-'], 
     "v4_no_injection": ["purple", '--'], 
     "resnet50_untrained": ["pink", '--'], 
@@ -62,11 +63,12 @@ list_metrics = {
 
 model_names = [
     #"injection_v1",
-    #"injection_v2", 
-    "injection_v4",
+    "injection_v2", 
+    #"injection_v4",
     #"injection_IT",
     #"injection_conv_v1", 
-    "injection_conv_v4", 
+    "injection_conv_v2", 
+    #"injection_conv_v4", 
     #"v4_no_injection", 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
