9f11f5d1eb4ffad1be7ce79343dbd8c1b4b247f3
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-12 15:36:12 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -93,22 +93,22 @@ model_names = [
     # random injection models  
     #"injection_v1",
     #"injection_v2", 
-    "injection_v4",
+    #"injection_v4",
     #"injection_IT",
     
     # convolution injection models 
-    #"injection_conv_v1", 
-    #"injection_conv_v2", 
+    "injection_conv_v1", 
+    "injection_conv_v2", 
     "injection_conv_v4", 
-    #"injection_conv_IT", 
+    "injection_conv_IT", 
     
     # unfreeze convolution injection models 
     #"unfreeze_injection_v1", 
     #"unfreeze_injection_v2", 
-    "unfreeze_injection_v4", 
+    #"unfreeze_injection_v4", 
     #"unfreeze_injection_IT", 
     
-    #"v4_no_injection", 
+    "v4_no_injection", 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
     "barlow_fact_no_injection"
@@ -160,7 +160,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/unfreeze_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/complete_convolution_frozen_{}.png'.format(key))
 
 
 
