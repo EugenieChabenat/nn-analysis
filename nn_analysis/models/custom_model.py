700e7cca4c6e3948b302147cd22080f56c626134
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-12 09:49:48 -0400

    Update custom_model.py

--- nn_analysis/models/custom_model.py
+++ nn_analysis/models/custom_model.py
@@ -75,11 +75,11 @@ def _get_custom_model(arch, path=None, extract_method=None, model_kwargs={}, dev
         param.requires_grad = False
         
     print(path+"/checkpoint.pth")
-    with open(path+"/checkpoint.pth.tar", 'rb') as f:
-        state_dict = torch.load(f, map_location="cpu")[state_dict_key]
+    with open(path+"/checkpoint.pth", 'rb') as f:
+        #state_dict = torch.load(f, map_location="cpu")[state_dict_key]
         # --
         #model = torch.nn.DataParallel(model).cuda()
-        """ckpt = torch.load(f, map_location="cpu")
+        ckpt = torch.load(f, map_location="cpu")
         #print('cktp: ', ckpt.keys())
         #state_dict = model.load_state_dict(ckpt["model"])
         state_dict = ckpt["model"]
@@ -98,7 +98,7 @@ def _get_custom_model(arch, path=None, extract_method=None, model_kwargs={}, dev
         #state_dict = model.load_state_dict(ckpt["model"])
         #state_dict = model.load_state_dict(state_d)
         #print(state_dict)
-        print('model------')"""
+        print('model------')
         print(model)
         # -- 
         
