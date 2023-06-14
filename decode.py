283a5aa89add119bf22b4db8eea62c8a0f372a97
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-14 11:22:28 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -54,15 +54,15 @@ dict_color = {
     #"injection_conv_IT": ["green", '-'], 
         
     # unfreeze convolution injection 
-    "unfreeze_injection_v1": ["green", '-'], 
-    "unfreeze_injection_v2": ["green", '-'], 
-    "unfreeze_injection_v4": ["green", '-'], 
-    "unfreeze_injection_IT": ["green", '-'], 
-    
-    #"unfreeze_injection_v1": ["red", '-'], 
-    #"unfreeze_injection_v2": ["blue", '-'], 
-    #"unfreeze_injection_v4": ["orange", '-'], 
+    #"unfreeze_injection_v1": ["green", '-'], 
+    #"unfreeze_injection_v2": ["green", '-'], 
+    #"unfreeze_injection_v4": ["green", '-'], 
     #"unfreeze_injection_IT": ["green", '-'], 
+    
+    "unfreeze_injection_v1": ["red", '-'], 
+    "unfreeze_injection_v2": ["blue", '-'], 
+    "unfreeze_injection_v4": ["orange", '-'], 
+    "unfreeze_injection_IT": ["green", '-'], 
     
     # control models 
     "v4_no_injection": ["purple", '--'], 
@@ -104,24 +104,24 @@ model_names = [
     #"injection_v1",
     #"injection_v2", 
     #"injection_v4",
-    "injection_IT",
+    #"injection_IT",
     
     # convolution injection models 
     #"injection_conv_v1", 
     #"injection_conv_v2", 
     #"injection_conv_v4", 
-    "injection_conv_IT", 
+    #"injection_conv_IT", 
     
     # unfreeze convolution injection models 
-    #"unfreeze_injection_v1", 
-    #"unfreeze_injection_v2", 
-    #"unfreeze_injection_v4", 
+    "unfreeze_injection_v1", 
+    "unfreeze_injection_v2", 
+    "unfreeze_injection_v4", 
     "unfreeze_injection_IT", 
     
-    "v4_no_injection", 
+    #"v4_no_injection", 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
-    "barlow_fact_no_injection"
+    #"barlow_fact_no_injection"
 ]
 
 """model_names = [
@@ -170,7 +170,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/IT_convolution_unfreeze_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/complete_convolution_unfreeze_{}.png'.format(key))
 
 
 
