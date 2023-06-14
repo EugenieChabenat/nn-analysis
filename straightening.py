14e97eb8097d546be18f9b7c3a239a41b19d2362
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-14 10:51:24 -0400

    Update straightening.py

--- straightening.py
+++ straightening.py
@@ -52,15 +52,15 @@ dict_color = {
     #"injection_conv_IT": ["green", '-'], 
         
     # unfreeze convolution injection 
-    "unfreeze_injection_v1": ["green", '-'], 
-    "unfreeze_injection_v2": ["green", '-'], 
-    "unfreeze_injection_v4": ["green", '-'], 
-    "unfreeze_injection_IT": ["green", '-'], 
-    
-    #"unfreeze_injection_v1": ["red", '-'], 
-    #"unfreeze_injection_v2": ["blue", '-'], 
-    #"unfreeze_injection_v4": ["orange", '-'], 
+    #"unfreeze_injection_v1": ["green", '-'], 
+    #"unfreeze_injection_v2": ["green", '-'], 
+    #"unfreeze_injection_v4": ["green", '-'], 
     #"unfreeze_injection_IT": ["green", '-'], 
+    
+    "unfreeze_injection_v1": ["red", '-'], 
+    "unfreeze_injection_v2": ["blue", '-'], 
+    "unfreeze_injection_v4": ["orange", '-'], 
+    "unfreeze_injection_IT": ["green", '-'], 
     
     # control models 
     "v4_no_injection": ["purple", '--'], 
@@ -117,26 +117,26 @@ list_metrics = {
 model_names = [
     # random injection models  
     #"injection_v1",
-    "injection_v2", 
+    #"injection_v2", 
     #"injection_v4",
     #"injection_IT",
     
     # convolution injection models 
     #"injection_conv_v1", 
-    "injection_conv_v2", 
+    #"injection_conv_v2", 
     #"injection_conv_v4", 
     #"injection_conv_IT", 
     
     # unfreeze convolution injection models 
-    #"unfreeze_injection_v1", 
-    #"unfreeze_injection_v2", 
-    #"unfreeze_injection_v4", 
+    "unfreeze_injection_v1", 
+    "unfreeze_injection_v2", 
+    "unfreeze_injection_v4", 
     "unfreeze_injection_IT", 
     
-    "v4_no_injection", 
+    "#v4_no_injection", 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
-    "barlow_fact_no_injection"
+    #"barlow_fact_no_injection"
 ]
 
 """model_names = [
@@ -189,7 +189,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/v2_convolution_unfreeze_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/complete_convolution_unfreeze_{}.png'.format(key))
 
 
 
