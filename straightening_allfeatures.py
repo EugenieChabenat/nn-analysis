412f94d9d012e632a07782134add32411474d94e
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 14:25:42 -0400

    Update straightening_allfeatures.py

--- straightening_allfeatures.py
+++ straightening_allfeatures.py
@@ -37,13 +37,13 @@ dict_color = {
     # random injection 
     "injection_v1_af" : ["orange", '-'],
     "injection_v2_af": ["orange", '-'], 
-    #"injection_v4_af": ["orange", '-'],
+    "injection_v4_af": ["orange", '-'],
     #"injection_IT_af": ["orange", '-'],
     
     # convolution injection
     "injection_conv_v1_af": ["red", '-'], 
     "injection_conv_v2_af": ["red", '-'], 
-    #"injection_conv_v4_af": ["red", '-'], 
+    "injection_conv_v4_af": ["red", '-'], 
     #"injection_conv_IT_af": ["red", '-'], 
     
     #"injection_conv_v1_af": ["red", '-'], 
@@ -54,7 +54,7 @@ dict_color = {
     # unfreeze convolution injection 
     "unfreeze_injection_v1_af": ["green", '-'], 
     "unfreeze_injection_v2_af": ["green", '-'], 
-    #"unfreeze_injection_v4_af": ["green", '-'], 
+    "unfreeze_injection_v4_af": ["green", '-'], 
     #"unfreeze_injection_IT_af": ["green", '-'], 
     
     #"unfreeze_injection_v1_af": ["red", '-'], 
@@ -66,7 +66,7 @@ dict_color = {
     # subset injection 
     "subset_injection_v1": ["blue", '-'], 
     "subset_injection_v2": ["blue", '-'], 
-    #"subset_injection_v4": ["blue", '-'], 
+    "subset_injection_v4": ["blue", '-'], 
     #"subset_injection_IT": ["blue", '-'], 
     
     # control models 
@@ -124,26 +124,26 @@ list_metrics = {
 model_names = [
     # random injection models  
     #"injection_v1_af",
-    "injection_v2_af", 
-    #"injection_v4_af",
+    #"injection_v2_af", 
+    "injection_v4_af",
     #"injection_IT_af",
     
     # convolution injection models 
     #"injection_conv_v1_af", 
-    "injection_conv_v2_af", 
-    #"injection_conv_v4_af", 
+    #"injection_conv_v2_af", 
+    "injection_conv_v4_af", 
     #"injection_conv_IT_af", 
     
     # unfreeze convolution injection models 
     #"unfreeze_injection_v1_af", 
-    "unfreeze_injection_v2_af", 
-    #"unfreeze_injection_v4_af", 
+    #"unfreeze_injection_v2_af", 
+    "unfreeze_injection_v4_af", 
     #"unfreeze_injection_IT_af", 
 
     # subset 
     #"subset_injection_v1", 
-    "subset_injection_v2", 
-    #"subset_injection_v4", 
+    #"subset_injection_v2", 
+    "subset_injection_v4", 
     #"subset_injection_IT", 
     
     #"v4_no_injection", 
@@ -202,7 +202,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/all_features_IT_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/v2_with_subset_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/v4_with_subset_{}.png'.format(key))
 
 
 
