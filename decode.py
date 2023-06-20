2aaf1a3c2057f5a65ff21091579e9b7d8d32da1a
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 14:30:40 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -108,26 +108,26 @@ list_metrics = {
 }
 model_names = [
     # random injection models  
-    "injection_v1",
-    #"injection_v2", 
+    #"injection_v1",
+    "injection_v2", 
     #"injection_v4",
     #"injection_IT",
     
     # convolution injection models 
-    "injection_conv_v1", 
-    #"injection_conv_v2", 
+    #"injection_conv_v1", 
+    "injection_conv_v2", 
     #"injection_conv_v4", 
     #"injection_conv_IT", 
     
     # unfreeze convolution injection models 
-    "unfreeze_injection_v1", 
-    #"unfreeze_injection_v2", 
+    #"unfreeze_injection_v1", 
+    "unfreeze_injection_v2", 
     #"unfreeze_injection_v4", 
     #"unfreeze_injection_IT", 
 
     # subset 
-    "subset_injection_v1", 
-    #"subset_injection_v2", 
+    #"subset_injection_v1", 
+    "subset_injection_v2", 
     #"subset_injection_v4", 
     #"subset_injection_IT",
     
@@ -183,7 +183,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/subset_v1_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/subset_v2_{}.png'.format(key))
 
 
 
