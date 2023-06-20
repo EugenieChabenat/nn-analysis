ef717778e01bc2789bd80cc27a251422656ab0e4
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 14:28:51 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -54,15 +54,15 @@ dict_color = {
     #"injection_conv_IT": ["green", '-'], 
         
     # unfreeze convolution injection 
-    #"unfreeze_injection_v1": ["green", '-'], 
-    #"unfreeze_injection_v2": ["green", '-'], 
-    #"unfreeze_injection_v4": ["green", '-'], 
-    #"unfreeze_injection_IT": ["green", '-'], 
-    
-    "unfreeze_injection_v1": ["red", '-'], 
-    "unfreeze_injection_v2": ["blue", '-'], 
-    "unfreeze_injection_v4": ["orange", '-'], 
+    "unfreeze_injection_v1": ["green", '-'], 
+    "unfreeze_injection_v2": ["green", '-'], 
+    "unfreeze_injection_v4": ["green", '-'], 
     "unfreeze_injection_IT": ["green", '-'], 
+    
+    #"unfreeze_injection_v1": ["red", '-'], 
+    #"unfreeze_injection_v2": ["blue", '-'], 
+    #"unfreeze_injection_v4": ["orange", '-'], 
+    #"unfreeze_injection_IT": ["green", '-'], 
 
     # subset injection 
     "subset_injection_v1": ["blue", '-'], 
@@ -108,27 +108,27 @@ list_metrics = {
 }
 model_names = [
     # random injection models  
-    #"injection_v1",
+    "injection_v1",
     #"injection_v2", 
     #"injection_v4",
     #"injection_IT",
     
     # convolution injection models 
-    #"injection_conv_v1", 
+    "injection_conv_v1", 
     #"injection_conv_v2", 
     #"injection_conv_v4", 
     #"injection_conv_IT", 
     
     # unfreeze convolution injection models 
     "unfreeze_injection_v1", 
-    "unfreeze_injection_v2", 
-    "unfreeze_injection_v4", 
-    "unfreeze_injection_IT", 
+    #"unfreeze_injection_v2", 
+    #"unfreeze_injection_v4", 
+    #"unfreeze_injection_IT", 
 
     # subset 
-    #"subset_injection_v1", 
+    "subset_injection_v1", 
     #"subset_injection_v2", 
-    "subset_injection_v4", 
+    #"subset_injection_v4", 
     #"subset_injection_IT",
     
     #"v4_no_injection", 
@@ -183,7 +183,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/bis_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/complete_convolution_unfreeze_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/subset_v1_{}.png'.format(key))
 
 
 
