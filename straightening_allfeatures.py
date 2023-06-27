fd70cab8a3e43e621d14999f15cff6b2b5bb75b9
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-27 11:24:28 -0400

    Update straightening_allfeatures.py

--- straightening_allfeatures.py
+++ straightening_allfeatures.py
@@ -215,14 +215,14 @@ for key, metric_types in list_metrics.items():
         axes[0,i].text(8, 0.2, "Block V2", ha="center", va="center", size=12)
         axes[0,i].text(13, 0.2, "Block V4", ha="center", va="center", size=12)
         axes[0,i].text(17.5, 0.2, "Block IT", ha="center", va="center", size=12)
-        axes[0,i].set_ylim(0.0, 1.)
+        #axes[0,i].set_ylim(0.0, 1.)
         axes[0,i].legend(loc='lower left')
     fig.supxlabel('layers')
     fig.supylabel('curvature')
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/v2_with_separate_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/controls_no_injection_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/no_ylim-controls_no_injection_{}.png'.format(key))
 
 
 
