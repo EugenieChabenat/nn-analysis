2d93ccf4c449723f28e2c576f5f596a19af15a4e
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-27 12:35:40 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -103,37 +103,37 @@ model_names = [
     # random injection models  
     #"injection_v1",
     #"injection_v2", 
-    "injection_v4",
-    #"injection_IT",
+    #"injection_v4",
+    "injection_IT",
     
     # convolution injection models 
     #"injection_conv_v1", 
     #"injection_conv_v2", 
-    "injection_conv_v4", 
-    #"injection_conv_IT", 
+    #"injection_conv_v4", 
+    "injection_conv_IT", 
     
     # unfreeze convolution injection models 
     #"unfreeze_injection_v1", 
     #"unfreeze_injection_v2", 
-    "unfreeze_injection_v4", 
-    #"unfreeze_injection_IT", 
+    #"unfreeze_injection_v4", 
+    "unfreeze_injection_IT", 
 
     # subset 
     #"subset_injection_v1", 
     #"subset_injection_v2", 
-    "subset_injection_v4", 
-    #"subset_injection_IT",
+    #"subset_injection_v4", 
+    "subset_injection_IT",
 
     # separate 
     #"injection_separate_v1", 
     #"injection_separate_v2", 
-    "injection_separate_v4", 
-    #"injection_separate_IT",
+    #"injection_separate_v4", 
+    "injection_separate_IT",
     
     #"v1_no_injection", 
     #"v2_no_injection", 
-    "v4_no_injection", 
-    #"IT_no_injection", 
+    #"v4_no_injection", 
+    "IT_no_injection", 
 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
@@ -186,7 +186,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/fact_v2_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/noylim-v4+_no_injection_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/noylim-IT+_no_injection_{}.png'.format(key))
 
 
 # ------------------------------------------------------------------------------------
