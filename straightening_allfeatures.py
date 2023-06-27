ccbecee24f9ee012e173445cd9128e7f0eb742ed
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-27 11:21:50 -0400

    Update straightening_allfeatures.py

--- straightening_allfeatures.py
+++ straightening_allfeatures.py
@@ -76,7 +76,11 @@ dict_color = {
     #"injection_separate_IT": ["purple", '-'], 
     
     # control models 
-    #"v4_no_injection": ["purple", '--'], 
+    "v1_no_injection": ["red", '--'], 
+    "v2_no_injection": ["blue", '--'], 
+    "v4_no_injection": ["green", '--'], 
+    "IT_no_injection": ["orange", '--'],
+    
     "resnet50_allfeatures": ["pink", '--'], 
     "bt_allfeatures": ["grey", '--'], 
     #"barlow_fact_no_injection": ["black", '--']
@@ -130,35 +134,39 @@ list_metrics = {
 model_names = [
     # random injection models  
     #"injection_v1_af",
-    "injection_v2_af", 
+    #"injection_v2_af", 
     #"injection_v4_af",
     #"injection_IT_af",
     
     # convolution injection models 
     #"injection_conv_v1_af", 
-    "injection_conv_v2_af", 
+    #"injection_conv_v2_af", 
     #"injection_conv_v4_af", 
     #"injection_conv_IT_af", 
     
     # unfreeze convolution injection models 
     #"unfreeze_injection_v1_af", 
-    "unfreeze_injection_v2_af", 
+    #"unfreeze_injection_v2_af", 
     #"unfreeze_injection_v4_af", 
     #"unfreeze_injection_IT_af", 
 
     # subset 
     #"subset_injection_v1", 
-    "subset_injection_v2", 
+    #"subset_injection_v2", 
     #"subset_injection_v4", 
     #"subset_injection_IT", 
 
     # separate
     #"injection_separate_v1", 
-    "injection_separate_v2", 
+    #"injection_separate_v2", 
     #"injection_separate_v4", 
     #"injection_separate_IT", 
 
-    #"v4_no_injection", 
+    "v1_no_injection", 
+    "v2_no_injection", 
+    "v4_no_injection", 
+    "IT_no_injection",
+    
     "resnet50_allfeatures", 
     "bt_allfeatures", 
     #"barlow_fact_no_injection"
@@ -201,7 +209,7 @@ for key, metric_types in list_metrics.items():
         
         axes[0,i].set_title(metric_type)
         axes[0,i].set_xticks([0, 3, 6, 10, 16, 19, 20])
-        axes[0,i].set_xticklabels(['', '', 'inj v1', 'inj v2', 'inj v4', 'inj IT', 20], rotation=45, ha='right')
+        axes[0,i].set_xticklabels(['convolution', 'maxpool', 'inj v1', 'inj v2', 'inj v4', 'inj IT', 'avgpool'], rotation=45, ha='right')
         
         axes[0,i].text(4.5, 0.2, "Block V1", ha="center", va="center", size=12)
         axes[0,i].text(8, 0.2, "Block V2", ha="center", va="center", size=12)
@@ -213,8 +221,8 @@ for key, metric_types in list_metrics.items():
     fig.supylabel('curvature')
     fig.tight_layout()
     plt.show()
-    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/v2_with_separate_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/v2_with_separate_{}.png'.format(key))
+    #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/v2_with_separate_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/controls_no_injection_{}.png'.format(key))
 
 
 
