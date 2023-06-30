790e2a87ce55210b469b306931fdd5aff2b9b141
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-30 12:35:46 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -125,46 +125,46 @@ list_metrics = {
 }
 model_names = [
     # random injection models  
-    #"injection_v1",
-    "injection_v2", 
+    "injection_v1",
+    #"injection_v2", 
     #"injection_v4",
     #"injection_IT",
     
     # convolution injection models 
-    #"injection_conv_v1", 
-    "injection_conv_v2", 
+    "injection_conv_v1", 
+    #"injection_conv_v2", 
     #"injection_conv_v4", 
     #"injection_conv_IT", 
     
     # unfreeze convolution injection models 
-    #"unfreeze_injection_v1", 
-    "unfreeze_injection_v2", 
+    "unfreeze_injection_v1", 
+    #"unfreeze_injection_v2", 
     #"unfreeze_injection_v4", 
     #"unfreeze_injection_IT", 
 
     # subset 
-    #"subset_injection_v1", 
-    "subset_injection_v2", 
+    "subset_injection_v1", 
+    #"subset_injection_v2", 
     #"subset_injection_v4", 
     #"subset_injection_IT",
 
     # conv subset injection 
-    #"injection_conv_subset_v1", 
-    "injection_conv_subset_v2", 
+    "injection_conv_subset_v1", 
+    #"injection_conv_subset_v2", 
     #"injection_conv_subset_v4", 
     #"injection_conv_subset_IT",
 
 
     # separate learning of weights 
-    #"injection_separate_v1", 
-    "injection_separate_v2", 
+    "injection_separate_v1", 
+    #"injection_separate_v2", 
     #"injection_separate_v4", 
     #"injection_separate_IT",
     
     
     # control models 
-    #"v1_no_injection", 
-    "v2_no_injection", 
+    "v1_no_injection", 
+    #"v2_no_injection", 
     #"v4_no_injection", 
     #"IT_no_injection", 
 
@@ -219,7 +219,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/decode_v2_separate_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/v2+_no_injection_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/v1+_no_injection_{}.png'.format(key))
 
 
 
