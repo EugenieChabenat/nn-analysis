1bf0b176f9b3b43e34246dc94b84a33deb499c73
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-02 16:17:27 -0400

    Update diff_straightening.py

--- diff_straightening.py
+++ diff_straightening.py
@@ -277,7 +277,7 @@ model_names = [
     #"barlow_fact_no_injection"
 ]
 
-fig, axes = pt.round_plot.subplots(1,4,height_per_plot=6,width_per_plot=6)
+fig, axes = pt.round_plot.subplots(1,3,height_per_plot=6,width_per_plot=6)
 for i, model_name in enumerate(model_names):
     #ys = [[results[model_name][metric][-1,0]-results[baseline_model_name][metric][-1,0] for metric in metrics] for metrics in metricss]
     ys = [[load_data(metric, model_name, epoch, one_layer[model_name])[metric_type] - load_data(metric, baseline_model[model_name], epoch, one_layer[model_name])[metric_type] for metric_type in metric_types] for metric_types in metricss]
