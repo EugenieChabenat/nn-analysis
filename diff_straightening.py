57f7274c9a14bf20d90ba2d31d5aba249f834687
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-03 14:23:52 -0400

    Update diff_straightening.py

--- diff_straightening.py
+++ diff_straightening.py
@@ -248,30 +248,30 @@ baseline_model = {"injection_v1_af": "v1_no_injection",
                   "injection_conv_subset_IT": "IT_no_injection" ,
                  "injection_separate_IT": "IT_no_injection"}
 
-one_layer = {"injection_v1": [6, 20],
-                  "injection_conv_v1" : [6, 20],
-                  "unfreeze_injection_v1": [6, 20], 
+one_layer = {"injection_v1_af": [6, 20],
+                  "injection_conv_v1_af" : [6, 20],
+                  "unfreeze_injection_v1_af": [6, 20], 
                   "subset_injection_v1": [6, 20], 
                   "injection_conv_subset_v1": [6, 20],
                  "injection_separate_v1": [6, 20], 
 
-             "injection_v2": [10, 20],
-                  "injection_conv_v2" : [10, 20],
-                  "unfreeze_injection_v2": [10, 20], 
+             "injection_v2_af": [10, 20],
+                  "injection_conv_v2_af" : [10, 20],
+                  "unfreeze_injection_v2_af": [10, 20], 
                   "subset_injection_v2": [10, 20], 
                   "injection_conv_subset_v2": [10, 20],
                  "injection_separate_v2": [10, 20], 
 
-             "injection_v4": [16, 20],
-                  "injection_conv_v4" : [16, 20],
-                  "unfreeze_injection_v4": [16, 20], 
+             "injection_v4_af": [16, 20],
+                  "injection_conv_v4_af" : [16, 20],
+                  "unfreeze_injection_v4_af": [16, 20], 
                   "subset_injection_v4": [16, 20], 
                   "injection_conv_subset_v4": [16, 20],
                  "injection_separate_v4": [16, 20], 
 
-             "injection_IT": [19, 20],
-                  "injection_conv_IT" : [19, 20],
-                  "unfreeze_injection_IT": [19, 20], 
+             "injection_IT_af": [19, 20],
+                  "injection_conv_IT_af" : [19, 20],
+                  "unfreeze_injection_IT_af": [19, 20], 
                   "subset_injection_IT": [19, 20], 
                   "injection_conv_subset_IT": [19, 20],
                  "injection_separate_IT": [19, 20], 
@@ -329,18 +329,18 @@ model_names = [
     "unfreeze_injection_v2_af", 
     "injection_conv_subset_v2",
 
-    #"injection_v4",
+    #"injection_v4_af",
     #"injection_separate_v4", 
     #"subset_injection_v4", 
-    #"injection_conv_v4",
-    #"unfreeze_injection_v4", 
+    #"injection_conv_v4_af",
+    #"unfreeze_injection_v4_af", 
     #"injection_conv_subset_v4",
 
-    #"injection_IT",
+    #"injection_IT_af",
     #"injection_separate_IT", 
     #"subset_injection_IT", 
-    #"injection_conv_IT",
-    #"unfreeze_injection_IT", 
+    #"injection_conv_IT_af",
+    #"unfreeze_injection_IT_af", 
     #"injection_conv_subset_IT",
     
     #"v4_no_injection", 
@@ -360,16 +360,7 @@ def grouped_bar(ax, xs, ys, ys_, alpha, colors, edgecolor, width=0.2, sep=0.3):
     fig2 = plt.subplots()
     for i, (y, y_) in enumerate(zip(ys, ys_)):
         xticks = np.linspace(0.0,len(y)*width,num=len(y))+total
-        """if (np.abs(y_) > np.abs(y) and y_ < 0 and y <0) or  (np.abs(y_) > np.abs(y) and y_ > 0 and y > 0): # both decrease and Y_ decreases more 
-            p1 = ax.bar(xticks, y, width=width, alpha = alpha, color =  colors[0], edgecolor = edgecolor)
-            p2 = ax.bar(xticks, (y_-y), bottom = y, width=width, alpha = alpha, color = colors[1], edgecolor = edgecolor)
-        # both decrease and y decreases more 
-        elif (np.abs(y_) < np.abs(y) and y_ < 0 and y <0) or (np.abs(y_) < np.abs(y) and y_ > 0 and y  >0) : # both decrease and Y_ decreases more 
-            p1 = ax.bar(xticks, (y-y_), bottom = y_, width=width, alpha = alpha, color =  colors[0], edgecolor = edgecolor)
-            p2 = ax.bar(xticks, y_, width=width, alpha = alpha, color = colors[1], edgecolor = edgecolor)
-        # they are signed different 
-        #elif (y_ < 0 and y > 0) or (y_ > 0 and y < 0):  
-        else: """
+        
         p1 = ax.bar(xticks, y, width=width, alpha = alpha, color =  colors[0], edgecolor = edgecolor)
         p2 = ax.bar(xticks, y_, width=width/2, alpha = alpha, color = colors[1], edgecolor = edgecolor)
 
@@ -418,6 +409,6 @@ for i in range(3):
 fig.suptitle('Comparison in straightening performance between injection V1 models and control (no injection) at injection site', fontweight='bold')
 fig.tight_layout()
 
-pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/straight_v2.png')
+pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/straight2_v2.png')
 pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/hist-compare_straightening_v2.png')
 fig.show()
