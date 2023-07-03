4ea6096ac73bb7beb33715c0fb6ef13f7d853bf4
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-03 11:03:45 -0400

    Update diff_straightening.py

--- diff_straightening.py
+++ diff_straightening.py
@@ -309,7 +309,6 @@ def grouped_bar(ax, xs, ys, ys_, alpha, colors, edgecolor, width=0.2, sep=0.3):
         all_xlabels += xs[i]
         
     ax.legend((p1[0], p2[0]), ('injection site', 'last layer'))
-    ax.show()
     ax.set_xticks(all_xticks)
     ax.set_xticklabels(all_xlabels, rotation=45, ha='right')
     
