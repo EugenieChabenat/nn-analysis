19953f212320d5de76a037a765b42ec856121b49
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-03 11:06:27 -0400

    Update diff_straightening.py

--- diff_straightening.py
+++ diff_straightening.py
@@ -304,10 +304,11 @@ def grouped_bar(ax, xs, ys, ys_, alpha, colors, edgecolor, width=0.2, sep=0.3):
         xticks = np.linspace(0.0,len(y)*width,num=len(y))+total
         p1 = ax.bar(xticks, y, width=width, alpha = alpha, color =  colors[0], edgecolor = edgecolor)
         p2 = ax.bar(xticks, y_, bottom = y, width=width, alpha = alpha, color = colors[1], edgecolor = edgecolor)
+        
         total += (len(y)+1.5)*width + sep
         all_xticks += list(xticks)
         all_xlabels += xs[i]
-        
+    ax.axhline(y=0, label='identity', color = 'grey', ls = '--')
     ax.legend((p1[0], p2[0]), ('injection site', 'last layer'))
     ax.set_xticks(all_xticks)
     ax.set_xticklabels(all_xlabels, rotation=45, ha='right')
@@ -337,6 +338,6 @@ for i in range(len(model_names)):
 fig.suptitle('Comparison in straightening performance between injection V1 models and control (no injection) at injection site', fontweight='bold')
 fig.tight_layout()
 
-pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/lookper10.png')
+pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/lookper10bis.png')
 #pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/compare_random_conv_last_layer.png')
 fig.show()
