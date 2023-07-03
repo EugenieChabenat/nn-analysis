a325c8e7bbeeac7b1a8a897bf2af7bf31751bdf9
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-03 14:42:43 -0400

    Update diff_fact.py

--- diff_fact.py
+++ diff_fact.py
@@ -289,19 +289,19 @@ one_layer = {"injection_v1": [6, 20],
 }
 
 model_names = [
-    #"injection_v1",
-    #"injection_separate_v1", 
-    #"subset_injection_v1", 
-    #"injection_conv_v1",
-    #"unfreeze_injection_v1", 
-    #"injection_conv_subset_v1",
+    "injection_v1",
+    "injection_separate_v1", 
+    "subset_injection_v1", 
+    "injection_conv_v1",
+    "unfreeze_injection_v1", 
+    "injection_conv_subset_v1",
 
-    "injection_v2",
-    "injection_separate_v2", 
-    "subset_injection_v2", 
-    "injection_conv_v2",
-    "unfreeze_injection_v2", 
-    "injection_conv_subset_v2",
+    #"injection_v2",
+    #"injection_separate_v2", 
+    #"subset_injection_v2", 
+    #"injection_conv_v2",
+    #"unfreeze_injection_v2", 
+    #"injection_conv_subset_v2",
 
     #"injection_v4",
     #"injection_separate_v4", 
@@ -412,10 +412,10 @@ y_lim_max = max(max([axes[0, i].get_ylim()[1] for i in range(3)]), max([axes[1, 
 for i in range(3):
     axes[0, i].set_ylim(y_lim_min, y_lim_max)
     axes[1, i].set_ylim(y_lim_min, y_lim_max)
-fig.suptitle('Comparison in factorization performance between injection V2 models and control (no injection) at injection site', fontweight='bold')
+fig.suptitle('Comparison in factorization performance between injection V1 models and control (no injection) at injection site', fontweight='bold')
 fig.tight_layout()
 
-pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/factpervv2.png')
-pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/hist-compare_fact_V2.png')
+pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/factpervv1.png')
+pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/hist-compare_fact_V1.png')
 fig.show()
 
