47af7127e775378e50ff5fda780be7edbeeca361
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-02 22:35:19 -0400

    Update diff_fact.py

--- diff_fact.py
+++ diff_fact.py
@@ -162,7 +162,7 @@ model_names = [
 # ------------------------------------------------------------------------------------
 # LAYERS PLOT 
 # ------------------------------------------------------------------------------------
-for key, metric_types in list_metrics.items(): 
+"""for key, metric_types in list_metrics.items(): 
     fig, axes = pt.core.subplots(1, len(metric_types), size=(10, 8), sharex=True)
     for i, metric_type in enumerate(metric_types):
         for model_name in model_names:
@@ -198,7 +198,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/fact_v2_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/IT+_no_injection_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/IT+_no_injection_{}.png'.format(key))"""
 
 
 # ------------------------------------------------------------------------------------
