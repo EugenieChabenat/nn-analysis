ac537743b7babeec0ed0572113a3150a90de6181
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-03 14:57:07 -0400

    Update diff_decoding.py

--- diff_decoding.py
+++ diff_decoding.py
@@ -298,19 +298,19 @@ one_layer = {"injection_v1": [6, 20],
 }
 
 model_names = [
-    #"injection_v1",
-    #"injection_separate_v1", 
-    #"subset_injection_v1", 
-    #"injection_conv_v1",
-    #"unfreeze_injection_v1", 
-    #"injection_conv_subset_v1",
+    "injection_v1",
+    "injection_separate_v1", 
+    "subset_injection_v1", 
+    "injection_conv_v1",
+    "unfreeze_injection_v1", 
+    "injection_conv_subset_v1",
 
-    "injection_v2",
-    "injection_separate_v2", 
-    "subset_injection_v2", 
-    "injection_conv_v2",
-    "unfreeze_injection_v2", 
-    "injection_conv_subset_v2",
+    #"injection_v2",
+    #"injection_separate_v2", 
+    #"subset_injection_v2", 
+    #"injection_conv_v2",
+    #"unfreeze_injection_v2", 
+    #"injection_conv_subset_v2",
 
     #"injection_v4",
     #"injection_separate_v4", 
@@ -414,11 +414,11 @@ y_lim_max = max(max([axes[0, i].get_ylim()[1] for i in range(3)]), max([axes[1, 
 for i in range(3):
     axes[0, i].set_ylim(y_lim_min, y_lim_max)
     axes[1, i].set_ylim(y_lim_min, y_lim_max)
-fig.suptitle('Comparison in decoding performance between injection V2 models and control (no injection) at injection site', fontweight='bold')
+fig.suptitle('Comparison in decoding performance between injection V1 models and control (no injection) at injection site', fontweight='bold')
 fig.tight_layout()
 
-pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/decode_v2.png')
-pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/hist-compare_decoding_v2.png')
+pt.round_plot.savefig(fig, '/home/ec3731/issa_analysis/nn-analysis/decode_v1.png')
+pt.round_plot.savefig(fig, '/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/hist-compare_decoding_v1.png')
 fig.show()
 
 
