88304603344ad77e4b960c7e81ab551f33765ac6
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-03 09:32:36 -0400

    Update diff_straightening.py

--- diff_straightening.py
+++ diff_straightening.py
@@ -255,7 +255,41 @@ one_layer = {"injection_v1_af": 6,
                   "injection_conv_v2": 20,
                   "injection_conv_v4" :20, 
                 "injection_conv_IT": 20}"""
-            
+dict_model_names = {
+    "injection_v1_af": "Random linear injection at V1",
+    "injection_separate_v1": "Trained linear injection at V1" , 
+    "injection_conv_v1_af": "Random convolutional injection at V1" ,
+    "unfreeze_injection_v1_af": "Trained convolutional injection at V1" , 
+    "subset_injection_v1": "Random linear injection of subset at V1", 
+    "injection_conv_subset_v1": "Random convolutional injection of subset at V1" ,
+
+    "injection_v2_af": "Random linear injection at V2",
+    "injection_separate_v2": "Trained linear injection at V2" , 
+    "injection_conv_v2_af": "Random convolutional injection at V2" ,
+    "unfreeze_injection_v2_af": "Trained convolutional injection at V2" , 
+    "subset_injection_v2": "Random linear injection of subset at V2", 
+    "injection_conv_subset_v2": "Random convolutional injection of subset at V2" ,
+
+    "injection_v4_af": "Random linear injection at V4",
+    "injection_separate_v4": "Trained linear injection at V4" , 
+    "injection_conv_v4_af": "Random convolutional injection at V4" ,
+    "unfreeze_injection_v4_af": "Trained convolutional injection at V4" , 
+    "subset_injection_v4": "Random linear injection of subset at V4", 
+    "injection_conv_subset_v4": "Random convolutional injection of subset at V4" ,
+
+    "injection_IT_af": "Random linear injection at IT",
+    "injection_separate_IT": "Trained linear injection at IT" , 
+    "injection_conv_IT_af": "Random convolutional injection at IT" ,
+    "unfreeze_injection_IT_af": "Trained convolutional injection at IT" , 
+    "subset_injection_IT": "Random linear injection of subset at IT", 
+    "injection_conv_subset_IT": "Random convolutional injection of subset at IT" ,
+    
+
+    #"v4_no_injection", 
+    #"resnet50_untrained", 
+    #"barlow_twins_50epochs", 
+    #"barlow_fact_no_injection"
+}    
 model_names = [
     "injection_v1_af",
     "injection_conv_v1_af",
@@ -288,7 +322,7 @@ for i, model_name in enumerate(model_names):
     print('ys:', len(ys))
     grouped_bar(axes[0,i], xs, ys)
     
-    axes[0,i].set_title(model_name)
+    axes[0,i].set_title(dict_model_name[model_name])
     axes[0,i].set_ylabel('Difference relative to baseline, in %')
 #     axes[0,i].set_ylim(-0.25,0.27)
 y_lim_min = min([axes[0,i].get_ylim()[0] for i in range(len(model_names))])
