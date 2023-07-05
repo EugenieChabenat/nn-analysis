72085a947080eee7529ccc0567ae7aac8034182a
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-04 20:16:51 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -245,17 +245,17 @@ for key, metric_types in list_metrics.items():
         axes[0,i].set_xticks([0, 3, 6, 10, 16, 19, 20])
         axes[0,i].set_xticklabels(['1st convolution', 'maxpool', 'v1 injection', 'v2 injection', 'v4 injection', 'IT injection', 'avgpool'], rotation=45, ha='right')
         
-        axes[0,i].text(4.5, 0.8, "Block V1", ha="center", va="center", size=12)
-        axes[0,i].text(8, 0.8, "Block V2", ha="center", va="center", size=12)
-        axes[0,i].text(13, 0.8, "Block V4", ha="center", va="center", size=12)
-        axes[0,i].text(17.5, 0.8, "Block IT", ha="center", va="center", size=12)
+        axes[0,i].text(4.5, 0.95, "Block V1", ha="center", va="center", size=12)
+        axes[0,i].text(8, 0.95, "Block V2", ha="center", va="center", size=12)
+        axes[0,i].text(13, 0.95, "Block V4", ha="center", va="center", size=12)
+        axes[0,i].text(17.5, 0.95, "Block IT", ha="center", va="center", size=12)
         axes[0,i].set_ylim(0.0, 1.)
-        axes[0,i].legend(loc='center left')
+        axes[0,i].legend()#loc='center left')
     fig.supxlabel('layers')
     fig.supylabel('decode')
     fig.tight_layout()
     plt.show()
-    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/decode_label_{}.png'.format(key))
+    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/decode_1label_{}.png'.format(key))
     #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/IT+_no_injection_{}.png'.format(key))
 
 
