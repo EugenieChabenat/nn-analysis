60e8715d0f1a324b01890ea79ce92f691a178a1b
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-07 14:03:07 -0400

    Update core.py

--- nn_analysis/acts/core.py
+++ nn_analysis/acts/core.py
@@ -50,7 +50,7 @@ def compute_sizes(model, layer_names, dataset, device='cpu'):
             x1 = model.backbone.maxpool(x1)
             x1 = model.backbone.layer1(x1)
             x1 = model.backbone.layer2(x1)
-            
+            x1 = model.backbone.layer3(x1)
             
             x1_ = model.backbone.avgpool(x1)
             #print('avgpool: ', x1_.shape)
@@ -62,7 +62,7 @@ def compute_sizes(model, layer_names, dataset, device='cpu'):
             z1 = model.bn(model.projector(x1_))
             
             #x1 = model.backbone.layer2(x1)
-            x1 = model.backbone.layer3(x1)
+            #x1 = model.backbone.layer3(x1)
             x1 = model.backbone.layer4(x1)
             
             
@@ -161,6 +161,7 @@ def save_dataset(filename, path, model, layer_names, dataset, device='cpu', batc
                 x1 = model.backbone.maxpool(x1)
                 x1 = model.backbone.layer1(x1)
                 x1 = model.backbone.layer2(x1)
+                x1 = model.backbone.layer3(x1)
                 
                 x1_ = model.backbone.avgpool(x1)
                 #x1_ = x1.reshape(x1_.shape[0], -1)
@@ -170,7 +171,7 @@ def save_dataset(filename, path, model, layer_names, dataset, device='cpu', batc
                 z1 = model.bn(model.projector(x1_))
                 
                 #x1 = model.backbone.layer2(x1)
-                x1 = model.backbone.layer3(x1)
+                #x1 = model.backbone.layer3(x1)
                 x1 = model.backbone.layer4(x1)
                 # --------- 
             
