d46eeef60a4add3a3246bfc16a716282902699d2
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-07 09:54:30 -0400

    Update straightening_condensed.py

--- straightening_condensed.py
+++ straightening_condensed.py
@@ -164,43 +164,43 @@ model_names = [
     #"injection_conv_subset_v1_proj", 
     
     # random injection models  
-    #"injection_v1_af",
-    "injection_v2_af", 
+    "injection_v1_af",
+    #"injection_v2_af", 
     #"injection_v4_af",
     #"injection_IT_af",
     
     # convolution injection models 
-    #"injection_conv_v1_af", 
-    "injection_conv_v2_af", 
+    "injection_conv_v1_af", 
+    #"injection_conv_v2_af", 
     #"injection_conv_v4_af", 
     #"injection_conv_IT_af", 
     
     # unfreeze convolution injection models 
-    #"unfreeze_injection_v1_af", 
-    "unfreeze_injection_v2_af", 
+    "unfreeze_injection_v1_af", 
+    #"unfreeze_injection_v2_af", 
     #"unfreeze_injection_v4_af", 
     #"unfreeze_injection_IT_af", 
 
     # subset 
-    #"subset_injection_v1", 
-    "subset_injection_v2", 
+    "subset_injection_v1", 
+    #"subset_injection_v2", 
     #"subset_injection_v4", 
     #"subset_injection_IT",
     
     # conv subset injection 
-    #"injection_conv_subset_v1", 
-    "injection_conv_subset_v2", 
+    "injection_conv_subset_v1", 
+    #"injection_conv_subset_v2", 
     #"injection_conv_subset_v4", 
     #"injection_conv_subset_IT",
     
     # separate
-    #"injection_separate_v1", 
-    "injection_separate_v2", 
+    "injection_separate_v1", 
+    #"injection_separate_v2", 
     #"injection_separate_v4", 
     #"injection_separate_IT", 
 
-    #"v1_no_injection", 
-    "v2_no_injection", 
+    "v1_no_injection", 
+    #"v2_no_injection", 
     #"v4_no_injection", 
     #"IT_no_injection",
     
@@ -247,7 +247,7 @@ for key, metric_types in list_metrics.items():
     fig.tight_layout()
     plt.show()
     #plt.savefig('/home/ec3731/issa_analysis/nn-analysis/d3-all-no_proj_IT_{}.png'.format(key))
-    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/no_proj/V2_{}.png'.format(key))
+    plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/no_proj/V1_{}.png'.format(key))
     #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_straightening_{}.png'.format(key))
     
 
