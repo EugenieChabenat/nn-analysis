275ee0d0a58fbc85a8d76a0f5d4bbd024160279d
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-08 13:13:17 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -265,7 +265,7 @@ for key, metric_types in list_metrics.items():
     fig.supylabel('decode')
     fig.tight_layout()
     plt.show()
-    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/2_v1_decode_{}.png'.format(key))
+    plt.savefig('/home/ec3731/issa_analysis/nn-analysis/3_v1_decode_{}.png'.format(key))
     #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/no_proj/noproj_IT_decode_{}.png'.format(key))
     #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_decode_{}.png'.format(key))
     
