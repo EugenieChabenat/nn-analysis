3a719a74d9ff4325754a353fa16d4f0d8752ea52
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-08 09:38:20 -0400

    Update main_save.py

--- bin/py_scripts/main_save.py
+++ bin/py_scripts/main_save.py
@@ -67,7 +67,7 @@ def main(model_name, layers, acts_name, version, epoch=None, overwrite=False, de
     # v2 
     #layers = [0, 1, 2, 3, 4, 5, 6, 16]
     
-    # v1 no proj 
+    # complete + projector layers 
     #layers = [0, 1, 2, 3, 4, 5, 6,7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
     
     # identity 
