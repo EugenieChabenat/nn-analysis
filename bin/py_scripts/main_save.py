a9877c114d6c06f6e9721eaf892e52683124c5bb
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-10 13:49:24 -0400

    Update main_save.py

--- bin/py_scripts/main_save.py
+++ bin/py_scripts/main_save.py
@@ -124,13 +124,13 @@ def main(model_name, layers, acts_name, version, epoch=None, overwrite=False, de
 if __name__ == '__main__':
     print("Started main_save.py...")
     parser = argparse.ArgumentParser(description='Save activations')
-    parser.add_argument('--model_name', default = "noprojector_linear_v1_nm3", type=str,
+    parser.add_argument('--model_name', default = "noprojector_linear_v1_nm6", type=str,
                         help='Specify the model of which activations are saved')
     parser.add_argument('--layers',default= 2, type=int, nargs='+',
                         help='Specify the layers of which activations are saved')
     parser.add_argument('--acts_name', default = "natural_movies", type=str,
                         help='Specify what activations are saved')
-    parser.add_argument('--version', default = 3,  type=int,
+    parser.add_argument('--version', default = 1,  type=int,
                         help='Version number. Error will be raised if the activations for the specified version' \
                              'already exists and --overwrite flag is not provided.')
     parser.add_argument('--epoch', type=int,
