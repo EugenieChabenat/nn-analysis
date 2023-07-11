a9348743e2580b47c671645af2045b941c9d6e1d
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-11 15:02:20 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -117,8 +117,8 @@ dict_metric_names = {
 }"""
 # factorization
 list_metrics = {
-    0: ['fact-background'], #, 'fact-obj_motion'],
-    1: ['fact-crop']#, 'fact-color']
+    0: ['fact-background'], 'fact-obj_motion'],
+    #1: ['fact-crop']#, 'fact-color']
 }
 # invariance
 """list_metrics = {
@@ -225,7 +225,7 @@ model_names = [
 # ------------------------------------------------------------------------------------
 # LAYERS PLOT 
 # ------------------------------------------------------------------------------------
-fig, axes = pt.core.subplots(1,1, size=(8, 6), sharex=True)
+fig, axes = pt.core.subplots(2,1, size=(8, 6), sharex=True)
 for key, metric_types in list_metrics.items(): 
     #fig, axes = pt.core.subplots(1, len(metric_types), size=(10, 8), sharex=True)
     for i, metric_type in enumerate(metric_types):
