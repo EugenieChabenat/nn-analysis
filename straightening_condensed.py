88ed205f405b0c48e0f8ccc6d08200b9da23c1a2
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-12 09:47:02 -0400

    Update straightening_condensed.py

--- straightening_condensed.py
+++ straightening_condensed.py
@@ -133,8 +133,8 @@ dict_metric_names = {
 }"""
 
 list_metrics = {
-    #0 : ["x_cam_trans", "y_cam_trans", "z_cam_trans"], 
-    0 : ["x_cam_rot", "y_cam_rot"], 
+    0 : ["x_cam_trans", "y_cam_trans", "z_cam_trans"], 
+    #0 : ["x_cam_rot", "y_cam_rot"], 
     #0 : ['x_cam_pan', 'yz_cam_pan']
 }
 
@@ -279,8 +279,8 @@ fig.supxlabel('layers')
 fig.supylabel('curvature')
 fig.tight_layout()
 plt.show()
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/IT-camrot-nop{}.png'.format(key))
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/IT_camrot_{}.png'.format(key))
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/IT-camtrans-nop{}.png'.format(key))
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/IT_camtrans_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_straightening_{}.png'.format(key))
     
 
