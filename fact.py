1a3ae0f4fb11e9faf483fe9ee225f963db8c2824
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-12 09:49:49 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -173,54 +173,54 @@ dict_model_names = {
 }
 model_names = [
     # trained without projector 
-    "noprojector_linear_v1", 
+    #"noprojector_linear_v1", 
     #"noprojector_linear_v2", 
     #"noprojector_linear_v4", 
     
     # random con - noprojector 
     #"noprojector_conv_v2", 
-    #"noprojector_conv_IT", 
+    "noprojector_conv_IT", 
     
     # random injection models  
-    "injection_v1",
+    #"injection_v1",
     #"injection_v2", 
     #"injection_v4",
-    #"injection_IT",
+    "injection_IT",
     
     # convolution injection models 
-    "injection_conv_v1", 
+    #"injection_conv_v1", 
     #"injection_conv_v2", 
     #"injection_conv_v4", 
-    #"injection_conv_IT", 
+    "injection_conv_IT", 
     
     # unfreeze convolution injection models 
-    "unfreeze_injection_v1", 
+    #"unfreeze_injection_v1", 
     #"unfreeze_injection_v2", 
     #"unfreeze_injection_v4", 
-    #"unfreeze_injection_IT", 
+    "unfreeze_injection_IT", 
 
     # subset 
-    "subset_injection_v1", 
+    #"subset_injection_v1", 
     #"subset_injection_v2", 
     #"subset_injection_v4", 
-    #"subset_injection_IT",
+    "subset_injection_IT",
 
     # conv subset injection 
-    "injection_conv_subset_v1", 
+    #"injection_conv_subset_v1", 
     #"injection_conv_subset_v2", 
     #"injection_conv_subset_v4", 
-    #"injection_conv_subset_IT",
+    "injection_conv_subset_IT",
 
     # separate 
-    "injection_separate_v1", 
+    #"injection_separate_v1", 
     #"injection_separate_v2", 
     #"injection_separate_v4", 
-    #"injection_separate_IT",
+    "injection_separate_IT",
     
-    "v1_no_injection", 
+    #"v1_no_injection", 
     #"v2_no_injection", 
     #"v4_no_injection", 
-    #"IT_no_injection", 
+    "IT_no_injection", 
 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
@@ -267,9 +267,9 @@ fig.supxlabel('layers')
 fig.supylabel('factorization')
 fig.tight_layout()
 plt.show()
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/v1-fact_{}.png'.format(key))
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/IT-fact_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_fact_{}.png'.format(key))
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/v1-fact_{}.png'.format(key))
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/IT-fact_{}.png'.format(key))
 
 
 # ------------------------------------------------------------------------------------
