9b31bc40d46594a87d2e00bb39a7f1697ec7a027
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-17 13:50:03 -0400

    Update average_decode.py

--- average_decode.py
+++ average_decode.py
@@ -286,6 +286,7 @@ for model_name in model_names:
         average_identity_scores = scores
         
   average_scores = [x/nb_metrics for x in average_scores]
+  average_identity_scores= [x/nb_metrics for x in average_identity_scores]
     
   plt.plot(layers, average_scores, label=dict_model_names[model_name], color = dict_color[model_name][0], ls = dict_color[model_name][1])
   plt.plot(layers, average_identity_scores, label='identity', color = 'black')
