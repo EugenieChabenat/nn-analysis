a75ae11484f51e9d018a6ffe7efebb84a74bb753
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-17 11:23:40 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -137,8 +137,8 @@ dict_model_names = {
     "injection_separate_v1": "Trained linear injection at V1" , 
     "injection_conv_v1": "Random convolutional injection at V1" ,
     "unfreeze_injection_v1": "Trained convolutional injection at V1" , 
-    "subset_injection_v1": "Random linear injection of subset at V1", 
-    "injection_conv_subset_v1": "Random convolutional injection of subset at V1" ,
+    "subset_injection_v1": "Random linear injection of spatial information at V1", 
+    "injection_conv_subset_v1": "Random convolutional injection of spatial information at V1" ,
     "noprojector_linear_v1": "Random linear injection at V1 - no projector", 
     "noprojector_conv_v1": "Random convolutional injection at V1 - no projector", 
 
@@ -146,8 +146,8 @@ dict_model_names = {
     "injection_separate_v2": "Trained linear injection at V2" , 
     "injection_conv_v2": "Random convolutional injection at V2" ,
     "unfreeze_injection_v2": "Trained convolutional injection at V2" , 
-    "subset_injection_v2": "Random linear injection of subset at V2", 
-    "injection_conv_subset_v2": "Random convolutional injection of subset at V2" ,
+    "subset_injection_v2": "Random linear injection of spatial information at V2", 
+    "injection_conv_subset_v2": "Random convolutional injection of spatial information at V2" ,
     "noprojector_linear_v2":  "Random linear injection at V2 - no projector",
     "noprojector_conv_v2":  "Random convolutional injection at V2 - no projector",
 
@@ -155,8 +155,8 @@ dict_model_names = {
     "injection_separate_v4": "Trained linear injection at V4" , 
     "injection_conv_v4": "Random convolutional injection at V4" ,
     "unfreeze_injection_v4": "Trained convolutional injection at V4" , 
-    "subset_injection_v4": "Random linear injection of subset at V4", 
-    "injection_conv_subset_v4": "Random convolutional injection of subset at V4" ,
+    "subset_injection_v4": "Random linear injection of spatial information at V4", 
+    "injection_conv_subset_v4": "Random convolutional injection of spatial at V4" ,
     "noprojector_linear_v4":  "Random linear injection at V4 - no projector",
     "noprojector_conv_v4":  "Random convolutional injection at V4 - no projector",
 
@@ -164,8 +164,8 @@ dict_model_names = {
     "injection_separate_IT": "Trained linear injection at IT" , 
     "injection_conv_IT": "Random convolutional injection at IT" ,
     "unfreeze_injection_IT": "Trained convolutional injection at IT" , 
-    "subset_injection_IT": "Random linear injection of subset at IT", 
-    "injection_conv_subset_IT": "Random convolutional injection of subset at IT", 
+    "subset_injection_IT": "Random linear injection of spatial information at IT", 
+    "injection_conv_subset_IT": "Random convolutional injection of spatial information at IT", 
     "noprojector_linear_IT": "Random linear injection at IT - no projector", 
     "noprojector_conv_IT": "Random convolutional injection at IT - no projector", 
     
