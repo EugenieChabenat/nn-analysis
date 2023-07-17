275d2e98e9035d5f7f09d2b4f5610106ca63d510
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-17 14:20:47 -0400

    Update average_decode.py

--- average_decode.py
+++ average_decode.py
@@ -217,44 +217,44 @@ model_names = [
     
     # random injection models  
     #"injection_v1",
-    "injection_v2", 
-    #"injection_v4",
+    #"injection_v2", 
+    "injection_v4",
     #"injection_IT",
     
     # convolution injection models 
     #"injection_conv_v1", 
-    "injection_conv_v2", 
-    #"injection_conv_v4", 
+    #"injection_conv_v2", 
+    "injection_conv_v4", 
     #"injection_conv_IT", 
     
     # unfreeze convolution injection models 
     #"unfreeze_injection_v1", 
-    "unfreeze_injection_v2", 
-    #"unfreeze_injection_v4", 
+    #"unfreeze_injection_v2", 
+    "unfreeze_injection_v4", 
     #"unfreeze_injection_IT", 
 
     # subset 
     #"subset_injection_v1", 
-    "subset_injection_v2", 
-    #"subset_injection_v4", 
+    #"subset_injection_v2", 
+    "subset_injection_v4", 
     #"subset_injection_IT",
 
     # conv subset injection 
     #"injection_conv_subset_v1", 
-    "injection_conv_subset_v2", 
-    #"injection_conv_subset_v4", 
+    #"injection_conv_subset_v2", 
+    "injection_conv_subset_v4", 
     #"injection_conv_subset_IT",
 
     # separate learning of weights 
     #"injection_separate_v1", 
-    "injection_separate_v2", 
-    #"injection_separate_v4", 
+    #"injection_separate_v2", 
+    "injection_separate_v4", 
     #"injection_separate_IT",
     
     # control models 
     #"v1_no_injection", 
-    "v2_no_injection", 
-    #"v4_no_injection", 
+    #"v2_no_injection", 
+    "v4_no_injection", 
     #"IT_no_injection", 
 
     "resnet50_untrained", 
@@ -302,8 +302,8 @@ for model_name in model_names:
 
 plt.axvline(x = 3, color = 'grey',  ls = 'dotted')
 plt.axvline(x = 6, color = 'grey', ls = 'dotted')
-plt.axvline(x = 10, color = 'red', ls = 'dotted', linewidth=4)
-plt.axvline(x = 16, color = 'grey',  ls = 'dotted')
+plt.axvline(x = 10, color = 'grey', ls = 'dotted')
+plt.axvline(x = 16, color = 'red',  ls = 'dotted', linewidth=4)
 plt.axvline(x = 19, color = 'grey', ls = 'dotted')
 plt.axvline(x = 20, color = 'grey' , ls = 'dotted')
 
@@ -326,7 +326,7 @@ plt.tick_params(axis='y', labelsize=14)
 #fig.tight_layout()
 plt.xlabel('layers', fontsize=14)
 plt.ylabel('average curvature score', fontsize=14)
-plt.title('Injection and evaluation at V1', fontsize=20)
+plt.title('Injection and evaluation at V4', fontsize=20)
 plt.show()
 plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/avg-curvature.png')
 
