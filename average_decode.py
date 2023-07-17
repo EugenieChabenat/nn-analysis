a3be847cd6d1402a3db87ba3587cc150aa7bfcd2
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-17 13:20:38 -0400

    Update average_decode.py

--- average_decode.py
+++ average_decode.py
@@ -303,7 +303,7 @@ plt.tick_params(axis='y', labelsize=14)
 #fig.tight_layout()
 plt.xlabel('layers')
 plt.ylabel('average decode')
-plt.title('Injection and evaluation at V4', fontsize=14)
+plt.title('Injection and evaluation at V4', fontsize=20)
 plt.show()
 plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/decode/avg-decode.png')
 
