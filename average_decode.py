806eaa4cfa555cfffc44da2c8afe169ffe5977c3
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-17 12:54:29 -0400

    Update average_decode.py

--- average_decode.py
+++ average_decode.py
@@ -267,7 +267,7 @@ nb_metrics = len(metric_types)
 
 average_scores = []
 
-fig, axes = pt.core.subplots(2, 2, size=(10, 10), sharex=True)
+#fig, axes = pt.core.subplots(2, 2, size=(10, 10), sharex=True)
 for model_name in model_names: 
   for i, metric_type in enumerate(metric_types): 
     scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
