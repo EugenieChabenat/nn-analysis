672457186e84251c004049d5e8d792bd8b08e3f1
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-17 10:53:32 -0400

    Update straightening_condensed_degrees.py

--- straightening_condensed_degrees.py
+++ straightening_condensed_degrees.py
@@ -156,8 +156,8 @@ model_names = [
 
 list_metrics = {
     #"Camera Translation" : ["x_cam_trans", "y_cam_trans", "z_cam_trans"], 
-    #"Camera Rotation" : ["y_cam_rot"], 
-    "Camera Pan" : [ 'yz_cam_pan'], 
+    "Camera Rotation" : ["y_cam_rot"], 
+    #"Camera Pan" : [ 'yz_cam_pan'], 
     
 }
 
