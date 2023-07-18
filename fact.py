12ca0682ae1e8fc9a8bcd90fb1c6020b46752074
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-18 16:28:36 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -252,8 +252,8 @@ model_names = [
 # ------------------------------------------------------------------------------------
 # LAYERS PLOT 
 # ------------------------------------------------------------------------------------
-#fig, axes = pt.core.subplots(2,2, size=(20, 10), sharex=True)
-fig, axes = pt.core.subplots(2,2, size=(10, 8), sharex=True)
+fig, axes = pt.core.subplots(2,2, size=(20, 10), sharex=True)
+#fig, axes = pt.core.subplots(2,2, size=(10, 8), sharex=True)
 for key, metric_types in list_metrics.items(): 
     #fig, axes = pt.core.subplots(1, len(metric_types), size=(10, 8), sharex=True)
     for i, metric_type in enumerate(metric_types):
@@ -284,13 +284,13 @@ for key, metric_types in list_metrics.items():
         axes[key,i].set_ylim(0.0, 1.0)
         axes[key,i].tick_params(axis='y', labelsize=14)
         
-        #axes[key,i].legend(loc='center left',bbox_to_anchor=(1.6, 0.5), fontsize=20)
+        axes[key,i].legend(loc='center left',bbox_to_anchor=(1.6, 0.5), fontsize=20)
 fig.supxlabel('layers')
 fig.supylabel('factorization')
 fig.tight_layout()
 plt.show()
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/it-legend_{}.png'.format(key))
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/g-it-fact-{}.png'.format(key))
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/g-it-legend-{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_fact_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/v4-fact_{}.png'.format(key))
 
