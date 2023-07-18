74d2b40eb3519ccf4d5b73fd0717e4aca68edefc
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-18 14:24:53 -0400

    Update straightening_condensed.py

--- straightening_condensed.py
+++ straightening_condensed.py
@@ -198,30 +198,30 @@ model_names = [
     #"noprojector_linear_v1_nm3",
 
     # no projector control 
-    #"noprojector_control_v1",
-    "noprojector_control_v2",
+    "noprojector_control_v1",
+    #"noprojector_control_v2",
     
     # no projector linear 
-    #"noprojector_linear_v1",
-    "noprojector_linear_v2", 
+    "noprojector_linear_v1",
+    #"noprojector_linear_v2", 
     #"noprojector_linear_v4", 
     #"noprojector_linear_IT", 
     
     # random conv no projector 
-    #"noprojector_conv_v1", 
-    "noprojector_conv_v2", 
+    "noprojector_conv_v1", 
+    #"noprojector_conv_v2", 
     #"noprojector_conv_v4",
     #"noprojector_conv_IT", 
     
     # random injection models  
-    #"injection_v1_af",
-    "injection_v2_af", 
+    "injection_v1_af",
+    #"injection_v2_af", 
     #"injection_v4_af",
     #"injection_IT_af",
     
     # convolution injection models 
-    #"injection_conv_v1_af", 
-    "injection_conv_v2_af", 
+    "injection_conv_v1_af", 
+    #"injection_conv_v2_af", 
     #"injection_conv_v4_af", 
     #"injection_conv_IT_af", 
     
@@ -249,8 +249,8 @@ model_names = [
     #"injection_separate_v4", 
     #"injection_separate_IT", 
 
-    #"v1_no_injection", 
-    "v2_no_injection", 
+    "v1_no_injection", 
+    #"v2_no_injection", 
     #"v4_no_injection", 
     #"IT_no_injection",
     
@@ -274,8 +274,8 @@ for key, metric_types in list_metrics.items():
         axes[key,i].plot(layers, scores, label='identity', color = 'black')
         
         axes[key,i].axvline(x = 3, color = 'grey',  ls = 'dotted')
-        axes[key,i].axvline(x = 6, color = 'grey', ls = 'dotted')
-        axes[key,i].axvline(x = 10, color = 'red', ls = 'dotted', linewidth=4)
+        axes[key,i].axvline(x = 6, color = 'red', ls = 'dotted', linewidth=4)
+        axes[key,i].axvline(x = 10, color = 'grey', ls = 'dotted')
         axes[key,i].axvline(x = 16, color = 'grey',  ls = 'dotted')
         axes[key,i].axvline(x = 19, color = 'grey', ls = 'dotted')
         axes[key,i].axvline(x = 20, color = 'grey' , ls = 'dotted')
@@ -296,7 +296,7 @@ fig.supxlabel('layers')
 fig.supylabel('curvature')
 fig.tight_layout()
 plt.show()
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/f-v2-campan-{}.png'.format(key))
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/f-v1-campan-{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/v4_campan_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_straightening_{}.png'.format(key))
     
