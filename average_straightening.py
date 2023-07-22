f73d39ead426698ceba481c3f9974ef5416f4cc7
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-22 09:34:32 -0400

    Update average_straightening.py

--- average_straightening.py
+++ average_straightening.py
@@ -206,32 +206,32 @@ model_names = [
     # control no projector
     #"noprojector_control_v1", 
     #"noprojector_control_v2",
-    #"noprojector_control_v4",
-    "noprojector_control_IT",
+    "noprojector_control_v4",
+    #"noprojector_control_IT",
     
     # random linear no projector
     #"noprojector_linear_v1", 
     #"noprojector_linear_v2",
-    #"noprojector_linear_v4", 
-    "noprojector_linear_IT", 
+    "noprojector_linear_v4", 
+    #"noprojector_linear_IT", 
     
     # random convolution no projector 
     #"noprojector_conv_v1", 
     #"noprojector_conv_v2",
-    #"noprojector_conv_v4", 
-    "noprojector_conv_IT", 
+    "noprojector_conv_v4", 
+    #"noprojector_conv_IT", 
     
     # random injection models  
     #"injection_v1_af",
     #"injection_v2_af", 
-    #"injection_v4_af",
-    "injection_IT_af",
+    "injection_v4_af",
+    #"injection_IT_af",
     
     # convolution injection models 
     #"injection_conv_v1_af", 
     #"injection_conv_v2_af", 
-    #"injection_conv_v4_af", 
-    "injection_conv_IT_af", 
+    "injection_conv_v4_af", 
+    #"injection_conv_IT_af", 
     
     # unfreeze convolution injection models 
     #"unfreeze_injection_v1_af", 
@@ -260,8 +260,8 @@ model_names = [
     # control models 
     #"v1_no_injection", 
     #"v2_no_injection", 
-    #"v4_no_injection", 
-    "IT_no_injection", 
+    "v4_no_injection", 
+    #"IT_no_injection", 
 
     "resnet50_untrained", 
     "barlow_twins_50epochs", 
@@ -306,8 +306,8 @@ for model_name in model_names:
 plt.axvline(x = 3, color = 'grey',  ls = 'dotted')
 plt.axvline(x = 6, color = 'grey', ls = 'dotted')
 plt.axvline(x = 10, color = 'grey', ls = 'dotted')
-plt.axvline(x = 16, color = 'grey',  ls = 'dotted')
-plt.axvline(x = 19, color = 'red', ls = 'dotted', linewidth=4)
+plt.axvline(x = 16, color = 'red',  ls = 'dotted', linewidth=4)
+plt.axvline(x = 19, color = 'grey', ls = 'dotted')
 plt.axvline(x = 20, color = 'grey' , ls = 'dotted')
 
 #axes[key,i].set_title(dict_metric_names[metric_type], fontsize=18)#, fontsize =60)
@@ -332,8 +332,8 @@ plt.ylabel('average curvature score', fontsize=14)
 #plt.title('Injection and evaluation at V1', fontsize=20)
 plt.title('Average curvature', fontsize=20)
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/avg-curve-IT.png')
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/avg-curve-IT.png')
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/avg-curve-v4.png')
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/avg-curve-v4.png')
 
 
     
