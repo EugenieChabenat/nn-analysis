32341c854ca0c985939222449a0fb78b86fee6ad
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-07-31 15:08:15 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -51,6 +51,12 @@ dict_color = {
     "noprojector_conv_v2": ["gold", '-'], 
     "noprojector_conv_v4": ["gold", '-'], 
     "noprojector_conv_IT": ["gold", '-'], 
+
+    # multiplicative injection 
+    "multiplicative_model_v1" : ["red", '-'], # '-'],
+    "multiplicative_model_v2": ["orange", '-'], # '-'],
+    "multiplicative_model_v4": ["blue",  '-'], # '-'],
+    "multiplicative_model_IT": ["green",  '-'], # '-'],
     
     # random injection 
     "injection_v1" : ["orange", ':'], #'-'],
@@ -158,7 +164,8 @@ dict_model_names = {
     "noprojector_linear_v1": "Random linear injection at V1 - no projector", 
     "noprojector_conv_v1": "Random convolutional injection at V1 - no projector", 
     "noprojector_control_v1": "Evaluation at V1, no injection - no projector", 
-
+    "multiplicative_model_v1" : "Random multiplicative injection at V1",
+    
     "injection_v2": "Random linear injection at V2",
     "injection_separate_v2": "Trained linear injection at V2" , 
     "injection_conv_v2": "Random convolutional injection at V2" ,
@@ -168,6 +175,7 @@ dict_model_names = {
     "noprojector_linear_v2":  "Random linear injection at V2 - no projector",
     "noprojector_conv_v2":  "Random convolutional injection at V2 - no projector",
     "noprojector_control_v2": "Evaluation at V2, no injection - no projector", 
+    "multiplicative_model_v2" : "Random multiplicative injection at V2",
 
     "injection_v4": "Random linear injection at V4",
     "injection_separate_v4": "Trained linear injection at V4" , 
@@ -178,6 +186,7 @@ dict_model_names = {
     "noprojector_linear_v4":  "Random linear injection at V4 - no projector",
     "noprojector_conv_v4":  "Random convolutional injection at V4 - no projector",
     "noprojector_control_v4": "Evaluation at V4, no injection - no projector", 
+    "multiplicative_model_v4" : "Random multiplicative injection at V4",
 
     "injection_IT": "Random linear injection at IT",
     "injection_separate_IT": "Trained linear injection at IT" , 
@@ -188,6 +197,7 @@ dict_model_names = {
     "noprojector_linear_IT": "Random linear injection at IT - no projector", 
     "noprojector_conv_IT": "Random convolutional injection at IT - no projector", 
     "noprojector_control_IT": "Evaluation at injection at IT, no injection - no projector",
+    "multiplicative_model_IT" : "Random multiplicative injection at IT",
     
     "v1_no_injection": "Evaluation at V1, no injection", 
     "v2_no_injection": "Evaluation at V2, no injection", 
@@ -199,8 +209,8 @@ dict_model_names = {
 }
 model_names = [
     # new architectures
-    "inj_v1_evaluate_IT", 
-    "inj_v2_evaluate_IT", 
+    #"inj_v1_evaluate_IT", 
+    #"inj_v2_evaluate_IT", 
     
     # no projector control 
     "noprojector_control_v1", 
@@ -215,19 +225,26 @@ model_names = [
     #"noprojector_linear_IT", 
     
     # random con - noprojector 
-    "noprojector_conv_v1", 
+    #"noprojector_conv_v1", 
     #"noprojector_conv_v2", 
     #"noprojector_conv_v4", 
     #"noprojector_conv_IT", 
     
+    # multiplicative models, 
+    "multiplicative_model_v1", 
+    "multiplicative_model_v2", 
+    "multiplicative_model_v4", 
+    "multiplicative_model_IT", 
+    
+    
     # random injection models  
-    "injection_v1",
+    #"injection_v1",
     #"injection_v2", 
     #"injection_v4",
     #"injection_IT",
     
     # convolution injection models 
-    "injection_conv_v1", 
+    #"injection_conv_v1", 
     #"injection_conv_v2", 
     #"injection_conv_v4", 
     #"injection_conv_IT", 
@@ -307,8 +324,8 @@ fig.supxlabel('layers')
 fig.supylabel('factorization')
 fig.tight_layout()
 plt.show()
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/v2it-legend-curve_{}.png'.format(key))
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/v2it-legend-curve_{}.png'.format(key))
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/multi-legend-curve_{}.png'.format(key))
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/multi-legend-curve_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_fact_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/fact/legend-all_{}.png'.format(key))
 
