9f10abe02e26ed9341a00160f8475c5c76ec3295
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-08-01 10:31:39 -0400

    Update straightening_condensed.py

--- straightening_condensed.py
+++ straightening_condensed.py
@@ -251,27 +251,27 @@ model_names = [
     #"noprojector_conv_IT", 
 
     # multiplicative models, 
-    "multiplicative_model_v1", 
-    #"multiplicative_model_v2", 
+    #"multiplicative_model_v1", 
+    "multiplicative_model_v2", 
     #"multiplicative_model_v4", 
     #"multiplicative_model_IT", 
 
     # injection into avgpool
-    "injection_avgpool_v1", 
-    #"injection_avgpool_v2", 
+    #"injection_avgpool_v1", 
+    "injection_avgpool_v2", 
     #"injection_avgpool_v4", 
     #"injection_avgpool_IT", 
     
     
     # random injection models  
-    "injection_v1_af",
-    #"injection_v2_af", 
+    #"injection_v1_af",
+    "injection_v2_af", 
     #"injection_v4_af",
     #"injection_IT_af",
     
     # convolution injection models 
-    "injection_conv_v1_af", 
-    #"injection_conv_v2_af", 
+    #"injection_conv_v1_af", 
+    "injection_conv_v2_af", 
     #"injection_conv_v4_af", 
     #"injection_conv_IT_af", 
     
@@ -299,8 +299,8 @@ model_names = [
     #"injection_separate_v4", 
     #"injection_separate_IT", 
 
-    "v1_no_injection", 
-    #"v2_no_injection", 
+    #"v1_no_injection", 
+    "v2_no_injection", 
     #"v4_no_injection", 
     #"IT_no_injection",
     
@@ -346,8 +346,8 @@ fig.supxlabel('layers')
 fig.supylabel('curvature')
 fig.tight_layout()
 plt.show()
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/avg-v1-campan-{}.png'.format(key))
-plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/avg-v1-campan_{}.png'.format(key))
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/avg-v2-campan-{}.png'.format(key))
+plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/straightening/avg-v2-campan_{}.png'.format(key))
 #plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/thesis_plots/nolegends_title/V1_straightening_{}.png'.format(key))
     
 
