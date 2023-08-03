582b14b1683635e7cbe11bfc4e620abb43e4fdac
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-08-03 09:50:45 -0400

    Update neural_fits.py

--- neural_fits.py
+++ neural_fits.py
@@ -11,10 +11,104 @@ def load_data(metric, model_name, epoch, layers):
         return [me.utils.load_data(model_name, epoch, layer_name, metric[0], metric[1]) for layer_name in layer_names]
     else:
         return me.utils.load_data(model_name, epoch, layer_names, metric[0], metric[1])
-      
+
+dict_color = {
+    # new architectures 
+    "inj_v1_evaluate_IT": ["magenta", '-'], 
+    "inj_v2_evaluate_IT": ["forestgreen", '-'], 
+    
+    # no projector control 
+    "noprojector_control_v1":  ["black", '-'], 
+    "noprojector_control_v2":  ["black", '-'], 
+    "noprojector_control_v4":  ["black", '-'], 
+    "noprojector_control_IT":  ["black", '-'], 
+    
+    # no projector linear 
+    "noprojector_linear_v1":  ["brown", '-'], 
+    "noprojector_linear_v2":  ["brown", '-'], 
+    "noprojector_linear_v4":  ["brown", '-'], 
+    "noprojector_linear_IT":  ["brown", '-'], 
+    
+    # no projector conv
+    "noprojector_conv_v1": ["gold", '-'], 
+    "noprojector_conv_v2": ["gold", '-'], 
+    "noprojector_conv_v4": ["gold", '-'], 
+    "noprojector_conv_IT": ["gold", '-'], 
+
+    # multiplicative injection 
+    "multiplicative_model_v1" : ["darkblue", '-'], # '-'],
+    "multiplicative_model_v2": ["darkblue", '-'], # '-'],
+    "multiplicative_model_v4": ["darkblue",  '-'], # '-'],
+    "multiplicative_model_IT": ["darkblue",  '-'], # '-'],
+
+    # multiplicative injection 
+    "injection_avgpool_v1" : ["forestgreen", '-'], # '-'],
+    "injection_avgpool_v2": ["forestgreen", '-'], # '-'],
+    "injection_avgpool_v4": ["forestgreen",  '-'], # '-'],
+    "injection_avgpool_IT": ["forestgreen",  '-'], # '-'],
+
+    # random injection 
+    "injection_v1" : ["orange", ':'], # '-'],
+    "injection_v2": ["orange", ':'], # '-'],
+    "injection_v4": ["orange",  ':'], # '-'],
+    "injection_IT": ["orange",  ':'], # '-'],
+    
+    # convolution injection
+    "injection_conv_v1": ["lightblue", ':'], # '-'],
+    "injection_conv_v2": ["lightblue",':'], # '-'],
+    "injection_conv_v4": ["lightblue", ':'], # '-'],
+    "injection_conv_IT": ["lightblue", ':'], # '-'],
+    
+    #"injection_conv_v1": ["red", '-'], 
+    #"injection_conv_v2": ["blue", '-'], 
+    #"injection_conv_v4": ["orange", '-'], 
+    #"injection_conv_IT": ["green", '-'], 
+        
+    # unfreeze convolution injection 
+    "unfreeze_injection_v1": ["green", '-'], 
+    "unfreeze_injection_v2": ["green", '-'], 
+    "unfreeze_injection_v4": ["green", '-'], 
+    "unfreeze_injection_IT": ["green", '-'], 
+    
+    #"unfreeze_injection_v1": ["red", '-'], 
+    #"unfreeze_injection_v2": ["blue", '-'], 
+    #"unfreeze_injection_v4": ["orange", '-'], 
+    #"unfreeze_injection_IT": ["green", '-'], 
+
+    # subset injection 
+    "subset_injection_v1": ["blue", '-'], 
+    "subset_injection_v2": ["blue", '-'], 
+    "subset_injection_v4": ["blue", '-'], 
+    "subset_injection_IT": ["blue", '-'],
+    
+    # separate injection 
+    "injection_separate_v1": ["purple", '-'], 
+    "injection_separate_v2": ["purple", '-'], 
+    "injection_separate_v4": ["purple", '-'], 
+    "injection_separate_IT": ["purple", '-'], 
+    
+    # conv subset injection 
+    "injection_conv_subset_v1": ["lime", '-'], 
+    "injection_conv_subset_v2": ["lime", '-'], 
+    "injection_conv_subset_v4": ["lime", '-'], 
+    "injection_conv_subset_IT": ["lime", '-'],
+
+    
+    # control models 
+    # control models 
+    "v1_no_injection": ["red", '--'], 
+    "v2_no_injection": ["red", '--'], 
+    "v4_no_injection": ["red", '--'], 
+    "IT_no_injection": ["red", '--'], 
+
+    "resnet50_untrained": ["pink", '--'], 
+    "barlow_twins_50epochs": ["grey", '--'], 
+    "barlow_fact_no_injection": ["black", '--']
+}
 epoch = 29
 layers = np.arange(2)
-layers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+#layers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+layers =[ 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 metric = ["neural_fits", 0]
 metric_types = ['hvm-V4', 'hvm-IT', 'rust-V4', 'rust-IT']
 
@@ -107,11 +201,14 @@ for i, metric_type in enumerate(metric_types):
     for model_name in model_names:
         
         scores = [load_data(metric, model_name, epoch, layer)[metric_type] for layer in layers]
-        axes[0,i].plot(layers, scores, label=model_name)
+        axes[0,i].plot(layers, scores, label=model_name, color = dict_color[model_name][0])
     #scores = [load_data(metric, 'identity', None, 0)[metric_type] for layer in layers]
     #axes[0,i].plot(layers, scores, label='identity')
     axes[0,i].set_title(metric_type)
-    axes[0,i].legend()
+    axes[0,i].set_xticks([6, 10, 16, 19, 20])
+    axes[0,i].set_xticklabels(['v1 injection', 'v2 injection', 'v4 injection', 'IT injection', 'avgpool'], rotation=45, ha='right', fontsize=16)#, fontsize=60)
+        
+    #axes[0,i].legend()
 fig.supxlabel('layers')
 fig.supylabel('neural fits')
 fig.tight_layout()
