3d9ec2c372e60d97b904ac35c497599121878dc7
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-08-18 14:27:24 -0400

    Update average_straightening.py

--- average_straightening.py
+++ average_straightening.py
@@ -212,26 +212,26 @@ dict_model_names = {
 }
 model_names = [
     # new architectures 
-    #"inj_v1_evaluate_IT", 
-    #"inj_v2_evaluate_IT", 
+    "inj_v1_evaluate_IT", 
+    "inj_v2_evaluate_IT", 
     
     # control no projector
     #"noprojector_control_v1", 
     #"noprojector_control_v2",
     #"noprojector_control_v4",
-    "noprojector_control_IT",
+    #"noprojector_control_IT",
     
     # random linear no projector
     #"noprojector_linear_v1", 
     #"noprojector_linear_v2",
     #"noprojector_linear_v4", 
-    "noprojector_linear_IT", 
+    #"noprojector_linear_IT", 
     
     # random convolution no projector 
     #"noprojector_conv_v1", 
     #"noprojector_conv_v2",
     #"noprojector_conv_v4", 
-    "noprojector_conv_IT", 
+    #"noprojector_conv_IT", 
     
     # random injection models  
     #"injection_v1_af",
@@ -322,7 +322,7 @@ plt.axvline(x = 3, color = 'grey',  ls = 'dotted')
 plt.axvline(x = 6, color = 'grey', ls = 'dotted')
 plt.axvline(x = 10, color = 'grey', ls = 'dotted')
 plt.axvline(x = 16, color = 'grey',  ls = 'dotted')
-plt.axvline(x = 19, color = 'red', ls = 'dotted', linewidth=4)
+plt.axvline(x = 19, color = 'grey', ls = 'dotted')#, linewidth=4)
 plt.axvline(x = 20, color = 'grey' , ls = 'dotted')
 
 #axes[key,i].set_title(dict_metric_names[metric_type], fontsize=18)#, fontsize =60)
@@ -345,10 +345,10 @@ plt.tick_params(axis='y', labelsize=14)
 plt.xlabel('layers', fontsize=14)
 plt.ylabel('average curvature score', fontsize=18)
 #plt.title('Injection and Evaluation at IT', fontsize=20)
-plt.title('Injection and Evaluation at IT', fontsize=30)
+plt.title('Average Curvature Score', fontsize=30)
 plt.show()
 plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/avg-curve-v12_pres.png')
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/pres-curve-it.png')
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/pres-curve-v12.png')
 
 
     
