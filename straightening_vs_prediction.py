1b99a154c4747c8a8485b99cb245629c6fecafa5
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-08-19 20:14:14 -0400

    Update straightening_vs_prediction.py

--- straightening_vs_prediction.py
+++ straightening_vs_prediction.py
@@ -199,11 +199,11 @@ markers = ['*', '*', '*', '*',
  'o', 'o']
     
 plt.figure(figsize=(15,15))
-plt.scatter(all_losses[:4], all_scores[:4], c=colors[:4], marker='*', s=30 )
-plt.scatter(all_losses[4:], all_scores[4:], c=colors[4:], marker='o' )
+plt.scatter(all_losses[:11], all_scores[:11], c=colors[:11], marker='*')
+plt.scatter(all_losses[11:], all_scores[11:], c=colors[11:], marker='o')
 #plt.scatter(all_losses[:-4], all_scores[:-4], c=colors[:-4], marker='*' , alpha = 0.5, s=16)
 plt.xlabel('prediction loss')
 plt.ylabel('average straightening score')
 plt.show()
-plt.savefig('/home/ec3731/issa_analysis/nn-analysis/scatter_loss_curve8.png')
+plt.savefig('/home/ec3731/issa_analysis/nn-analysis/scatter_loss_curve9.png')
 plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/scatter_loss_curve.png')
